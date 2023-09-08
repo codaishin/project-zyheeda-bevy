d4aa5b2205d90d2c8ed8e15b47df1b0c412f5d64
codaishin <codaishin@gmail.com>
2023-09-08 22:38:14 +0200

    feat(traits): GetWorldPosition for MousePositionEvent

--- src/traits/world_position/mouse_position_event.rs
+++ src/traits/world_position/mouse_position_event.rs
@@ -1,16 +1,24 @@
 #[cfg(test)]
+mod get_world_position_tests;
+
+#[cfg(test)]
 mod set_world_position_tests;
 
+use crate::events::MousePositionEvent;
 use bevy::prelude::*;
 
-use crate::events::MousePositionEvent;
-
-use super::SetWorldPositionFromRay;
+use super::{GetWorldPosition, SetWorldPositionFromRay};
 
 impl SetWorldPositionFromRay for MousePositionEvent {
 	fn set_world_position(&mut self, ray_cast: Ray) {
 		self.world_position = ray_cast
 			.intersect_plane(self.collision_plane.origin, self.collision_plane.normal)
 			.map(|distance| ray_cast.origin + ray_cast.direction * distance);
+	}
+}
+
+impl GetWorldPosition for MousePositionEvent {
+	fn get_world_position(&self) -> Option<Vec3> {
+		self.world_position
 	}
 }
