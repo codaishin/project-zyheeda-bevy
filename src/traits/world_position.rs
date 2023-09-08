3671cf28e1e2b207d4be04289008b3094203882a
codaishin <codaishin@gmail.com>
2023-09-08 22:03:30 +0200

    feat(systems): sending mouse events for movement

--- src/traits/world_position.rs
+++ src/traits/world_position.rs
@@ -1,5 +1,9 @@
-use bevy::prelude::Vec3;
+use bevy::prelude::*;
 
 pub trait GetWorldPosition {
 	fn get_world_position(&self) -> Option<Vec3>;
+}
+
+pub trait SetWorldPositionFromRay {
+	fn set_world_position(&mut self, ray_cast: Ray);
 }
