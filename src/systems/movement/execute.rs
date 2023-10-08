13ee4e3d88449fb302a0de48e1360a389203ed41
codaishin <codaishin@gmail.com>
2023-10-08 16:46:37 +0200

    feat(systems): execute also syncs movement mode

--- src/systems/movement/execute.rs
+++ src/systems/movement/execute.rs
@@ -19,10 +19,12 @@ pub fn execute<
 		return;
 	};
 
-	let speed = match mode {
+	let speed = match player.movement_mode {
 		MovementMode::Walk => player.movement_speed.to_f32(),
 		MovementMode::Run => player.run_speed.to_f32(),
 	};
+
+	*mode = player.movement_mode; //FIXME: not sure if we'd better use a separate system for this...
 	movement.update(&mut transform, time.delta_seconds() * speed);
 }
 
@@ -44,11 +46,11 @@ mod move_player_tests {
 	}
 
 	#[derive(Component)]
-	struct _State {
+	struct _Behaviors {
 		movement: Option<(Mock_Movement, MovementMode)>,
 	}
 
-	impl GetMut<(Mock_Movement, MovementMode)> for _State {
+	impl GetMut<(Mock_Movement, MovementMode)> for _Behaviors {
 		fn get(&mut self) -> Option<&mut (Mock_Movement, MovementMode)> {
 			self.movement.as_mut()
 		}
@@ -61,7 +63,7 @@ mod move_player_tests {
 		time.update();
 		app.insert_resource(time);
 		app.update();
-		app.add_systems(Update, execute::<Mock_Movement, _State>);
+		app.add_systems(Update, execute::<Mock_Movement, _Behaviors>);
 
 		app
 	}
@@ -89,7 +91,7 @@ mod move_player_tests {
 
 		time.update_with_instant(last_update + time_delta);
 		app.world.spawn((
-			_State {
+			_Behaviors {
 				movement: Some((movement, MovementMode::Walk)),
 			},
 			player,
@@ -113,7 +115,7 @@ mod move_player_tests {
 		movement.expect_update().times(2).return_const(());
 
 		app.world.spawn((
-			_State {
+			_Behaviors {
 				movement: Some((movement, MovementMode::Walk)),
 			},
 			player,
@@ -134,7 +136,7 @@ mod move_player_tests {
 		let player = Player {
 			movement_speed: UnitsPerSecond::new(5.),
 			run_speed: UnitsPerSecond::new(10.),
-			movement_mode: MovementMode::Walk,
+			movement_mode: MovementMode::Run,
 		};
 		let time_delta = Duration::from_millis(30);
 		let mut movement = Mock_Movement::new();
@@ -147,7 +149,7 @@ mod move_player_tests {
 
 		time.update_with_instant(last_update + time_delta);
 		app.world.spawn((
-			_State {
+			_Behaviors {
 				movement: Some((movement, MovementMode::Run)),
 			},
 			player,
@@ -155,5 +157,104 @@ mod move_player_tests {
 		));
 
 		app.update();
+	}
+
+	#[test]
+	fn prefer_player_movement_mode() {
+		let mut app = setup_app();
+		let mut time = app.world.resource_mut::<Time>();
+
+		let last_update = time.last_update().unwrap();
+		let transform = Transform::from_xyz(1., 2., 3.);
+		let player = Player {
+			movement_speed: UnitsPerSecond::new(5.),
+			run_speed: UnitsPerSecond::new(10.),
+			movement_mode: MovementMode::Run,
+		};
+		let time_delta = Duration::from_millis(30);
+		let mut movement = Mock_Movement::new();
+
+		movement
+			.expect_update()
+			.with(eq(transform), eq(time_delta.as_secs_f32() * 10.))
+			.times(1)
+			.return_const(());
+
+		time.update_with_instant(last_update + time_delta);
+		app.world.spawn((
+			_Behaviors {
+				movement: Some((movement, MovementMode::Walk)),
+			},
+			player,
+			transform,
+		));
+
+		app.update();
+	}
+
+	#[test]
+	fn update_movement_mode_from_player_walk() {
+		let mut app = setup_app();
+
+		let transform = Transform::from_xyz(1., 2., 3.);
+		let player = Player {
+			movement_speed: UnitsPerSecond::new(5.),
+			run_speed: UnitsPerSecond::new(10.),
+			movement_mode: MovementMode::Walk,
+		};
+		let mut movement = Mock_Movement::new();
+
+		movement.expect_update().return_const(());
+
+		let player = app
+			.world
+			.spawn((
+				_Behaviors {
+					movement: Some((movement, MovementMode::Run)),
+				},
+				player,
+				transform,
+			))
+			.id();
+
+		app.update();
+
+		let behaviors = app.world.entity(player).get::<_Behaviors>().unwrap();
+		let mode = behaviors.movement.as_ref().unwrap().1;
+
+		assert_eq!(MovementMode::Walk, mode);
+	}
+
+	#[test]
+	fn update_movement_mode_from_player_run() {
+		let mut app = setup_app();
+
+		let transform = Transform::from_xyz(1., 2., 3.);
+		let player = Player {
+			movement_speed: UnitsPerSecond::new(5.),
+			run_speed: UnitsPerSecond::new(10.),
+			movement_mode: MovementMode::Run,
+		};
+		let mut movement = Mock_Movement::new();
+
+		movement.expect_update().return_const(());
+
+		let player = app
+			.world
+			.spawn((
+				_Behaviors {
+					movement: Some((movement, MovementMode::Walk)),
+				},
+				player,
+				transform,
+			))
+			.id();
+
+		app.update();
+
+		let behaviors = app.world.entity(player).get::<_Behaviors>().unwrap();
+		let mode = behaviors.movement.as_ref().unwrap().1;
+
+		assert_eq!(MovementMode::Run, mode);
 	}
 }
