e44ef736e566d8782f746ece5043810536d56ca2
codaishin <codaishin@gmail.com>
2023-10-20 17:55:26 +0200

    feat(traits): impl player speeds

--- src/traits/speed/player/run.rs
+++ src/traits/speed/player/run.rs
@@ -6,6 +6,26 @@ use crate::{
 
 impl Speed<Run> for Player {
 	fn get_speed(&self) -> UnitsPerSecond {
-		todo!()
+		self.run_speed
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use bevy::utils::default;
+
+	fn get_player(speed: f32) -> impl Speed<Run> {
+		Player {
+			run_speed: UnitsPerSecond::new(speed),
+			..default()
+		}
+	}
+
+	#[test]
+	fn get_run_speed() {
+		let player = get_player(42.);
+
+		assert_eq!(UnitsPerSecond::new(42.), player.get_speed());
 	}
 }
--- src/traits/speed/player/walk.rs
+++ src/traits/speed/player/walk.rs
@@ -6,6 +6,26 @@ use crate::{
 
 impl Speed<Walk> for Player {
 	fn get_speed(&self) -> UnitsPerSecond {
-		todo!()
+		self.walk_speed
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use bevy::utils::default;
+
+	fn get_player(speed: f32) -> impl Speed<Walk> {
+		Player {
+			walk_speed: UnitsPerSecond::new(speed),
+			..default()
+		}
+	}
+
+	#[test]
+	fn get_walk_speed() {
+		let player = get_player(42.);
+
+		assert_eq!(UnitsPerSecond::new(42.), player.get_speed());
 	}
 }
