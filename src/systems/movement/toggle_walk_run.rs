7c8d15f272deb02e2330a9f0d5314e044dab132c
codaishin <codaishin@gmail.com>
2023-10-20 22:08:27 +0200

    feat(systems): movement mode toggle tracked on player again

--- src/systems/movement/toggle_walk_run.rs
+++ src/systems/movement/toggle_walk_run.rs
@@ -1,31 +1,15 @@
-use crate::{
-	behavior::{Run, Walk},
-	components::Player,
-};
+use crate::{behavior::MovementMode, components::Player};
 use bevy::prelude::*;
 
-type PlayerData<'a> = (Entity, Option<&'a Walk>, Option<&'a Run>);
-
-pub fn player_toggle_walk_run(
-	mut commands: Commands,
-	player: Query<PlayerData, With<Player>>,
-	keys: Res<Input<KeyCode>>,
-) {
+pub fn player_toggle_walk_run(mut player: Query<&mut Player>, keys: Res<Input<KeyCode>>) {
 	if !keys.just_pressed(KeyCode::NumpadSubtract) {
 		return;
 	}
 
-	for (player, walk, run) in player.iter() {
-		let mut player = commands.entity(player);
-		match (walk, run) {
-			(Some(_), None) => {
-				player.insert(Run);
-				player.remove::<Walk>();
-			}
-			_ => {
-				player.insert(Walk);
-				player.remove::<Run>();
-			}
+	for mut player in player.iter_mut() {
+		player.movement_mode = match player.movement_mode {
+			MovementMode::Walk => MovementMode::Run,
+			MovementMode::Run => MovementMode::Walk,
 		}
 	}
 }
@@ -33,15 +17,18 @@ pub fn player_toggle_walk_run(
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::components::Player;
+	use crate::{behavior::MovementMode, components::Player};
 
 	#[test]
 	fn toggle_player_walk_to_run() {
 		let mut app = App::new();
 		let keys = Input::<KeyCode>::default();
-		let player = Player { ..default() };
+		let player = Player {
+			movement_mode: MovementMode::Walk,
+			..default()
+		};
 
-		let player = app.world.spawn((player, Walk)).id();
+		let player = app.world.spawn(player).id();
 		app.add_systems(Update, player_toggle_walk_run);
 		app.insert_resource(keys);
 		app.world
@@ -50,20 +37,20 @@ mod tests {
 
 		app.update();
 
-		let player = app.world.entity(player);
-		assert_eq!(
-			(false, true),
-			(player.contains::<Walk>(), player.contains::<Run>())
-		);
+		let player = app.world.entity(player).get::<Player>().unwrap();
+		assert_eq!(MovementMode::Run, player.movement_mode);
 	}
 
 	#[test]
 	fn toggle_player_run_to_walk() {
 		let mut app = App::new();
 		let keys = Input::<KeyCode>::default();
-		let player = Player { ..default() };
+		let player = Player {
+			movement_mode: MovementMode::Run,
+			..default()
+		};
 
-		let player = app.world.spawn((player, Run)).id();
+		let player = app.world.spawn(player).id();
 		app.add_systems(Update, player_toggle_walk_run);
 		app.insert_resource(keys);
 		app.world
@@ -72,29 +59,26 @@ mod tests {
 
 		app.update();
 
-		let player = app.world.entity(player);
-		assert_eq!(
-			(true, false),
-			(player.contains::<Walk>(), player.contains::<Run>())
-		);
+		let player = app.world.entity(player).get::<Player>().unwrap();
+		assert_eq!(MovementMode::Walk, player.movement_mode);
 	}
 
 	#[test]
 	fn no_toggle_when_no_input() {
 		let mut app = App::new();
 		let keys = Input::<KeyCode>::default();
-		let player = Player { ..default() };
+		let player = Player {
+			movement_mode: MovementMode::Walk,
+			..default()
+		};
 
-		let player = app.world.spawn((player, Walk)).id();
+		let player = app.world.spawn(player).id();
 		app.add_systems(Update, player_toggle_walk_run);
 		app.insert_resource(keys);
 
 		app.update();
 
-		let player = app.world.entity(player);
-		assert_eq!(
-			(true, false),
-			(player.contains::<Walk>(), player.contains::<Run>())
-		);
+		let player = app.world.entity(player).get::<Player>().unwrap();
+		assert_eq!(MovementMode::Walk, player.movement_mode);
 	}
 }
