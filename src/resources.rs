f37ceadf6af7e0d4b648f47efe7e4a9b55839cf6
codaishin <codaishin@gmail.com>
2024-02-08 17:13:26 +0100

    feat(resource): Add `Shared`

--- src/resources.rs
+++ src/resources.rs
@@ -12,7 +12,14 @@ use bevy::{
 	render::{mesh::Mesh, texture::Image},
 	scene::Scene,
 };
-use std::{collections::HashMap, hash::Hash, marker::PhantomData};
+use std::{
+	collections::{
+		hash_map::Entry::{Occupied, Vacant},
+		HashMap,
+	},
+	hash::Hash,
+	marker::PhantomData,
+};
 
 #[derive(Resource)]
 pub struct Animations<T>(pub HashMap<T, Handle<AnimationClip>>);
@@ -185,5 +192,84 @@ impl<TFor, TParent, TChildren> Prefab<TFor, TParent, TChildren> {
 			children,
 			phantom_data: PhantomData,
 		}
+	}
+}
+
+#[derive(Resource)]
+pub struct Shared<TKey: Eq + Hash, T: Clone> {
+	map: HashMap<TKey, T>,
+}
+
+impl<TKey: Eq + Hash, T: Clone> Default for Shared<TKey, T> {
+	fn default() -> Self {
+		Self {
+			map: HashMap::default(),
+		}
+	}
+}
+
+impl<TKey: Eq + Hash, T: Clone> Shared<TKey, T> {
+	pub fn get_handle(&mut self, key: TKey, mut new_handle: impl FnMut() -> T) -> T {
+		match self.map.entry(key) {
+			Occupied(entry) => entry.get().clone(),
+			Vacant(entry) => entry.insert(new_handle()).clone(),
+		}
+	}
+
+	pub fn get(&self, key: &TKey) -> Option<&T> {
+		self.map.get(key)
+	}
+}
+
+#[cfg(test)]
+mod test_shared_asset {
+	use super::*;
+	use bevy::{asset::AssetId, utils::Uuid};
+
+	#[test]
+	fn get_new() {
+		let mut called = false;
+		let new_handle = Handle::<Mesh>::Weak(AssetId::Uuid {
+			uuid: Uuid::new_v4(),
+		});
+		let mut shared_assets = Shared::<u32, Handle<Mesh>>::default();
+		let handle = shared_assets.get_handle(42, || {
+			called = true;
+			new_handle.clone()
+		});
+
+		assert_eq!((new_handle, true), (handle, called));
+	}
+
+	#[test]
+	fn get_registered() {
+		let mut called = false;
+		let old_handle = Handle::<Mesh>::Weak(AssetId::Uuid {
+			uuid: Uuid::new_v4(),
+		});
+		let mut shared_assets = Shared::<u32, Handle<Mesh>>::default();
+		_ = shared_assets.get_handle(42, || old_handle.clone());
+		let handle = shared_assets.get_handle(42, || {
+			called = true;
+			Handle::default()
+		});
+
+		assert_eq!((old_handle, false), (handle, called));
+	}
+
+	#[test]
+	fn get_new_on_different_key() {
+		let mut called = false;
+		let new_handle = Handle::<Mesh>::Weak(AssetId::Uuid {
+			uuid: Uuid::new_v4(),
+		});
+		let mut shared_assets = Shared::<u32, Handle<Mesh>>::default();
+		_ = shared_assets.get_handle(42, Handle::default);
+		let handle = shared_assets.get_handle(43, || {
+			called = true;
+			new_handle.clone()
+		});
+
+		assert_eq!((new_handle, true), (handle, called));
 	}
 }
