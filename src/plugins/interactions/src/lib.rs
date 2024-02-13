206ddcf8f5712522c19351844b952399eba55339
codaishin <codaishin@gmail.com>
2024-02-13 14:54:25 +0100

    feat(systems): destroyer system

--- src/plugins/interactions/src/lib.rs
+++ src/plugins/interactions/src/lib.rs
@@ -1,12 +1,14 @@
+mod components;
 mod systems;
 
-use bevy::app::{App, Plugin, PostUpdate};
-use systems::collision::destroy_on_collision::destroy_on_collision;
+use bevy::app::{App, Plugin, PostUpdate, Update};
+use systems::{collision::destroy_on_collision::destroy_on_collision, destroy::destroy};
 
 pub struct InteractionsPlugin;
 
 impl Plugin for InteractionsPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(PostUpdate, destroy_on_collision);
+		app.add_systems(Update, destroy)
+			.add_systems(PostUpdate, destroy_on_collision);
 	}
 }
