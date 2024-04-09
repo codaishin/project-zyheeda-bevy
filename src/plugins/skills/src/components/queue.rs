7fd615fe68b0cd3c0522ba18022b61559b7813ae
codaishin <codaishin@gmail.com>
2024-04-09 15:03:59 +0200

    feat(skills): `Queue` states

--- src/plugins/skills/src/components/queue.rs
+++ src/plugins/skills/src/components/queue.rs
@@ -2,39 +2,82 @@ use crate::{
 	skill::{Queued, Skill},
 	traits::{Dequeue, Enqueue, Iter, IterMut},
 };
-use bevy::ecs::component::Component;
-use std::collections::VecDeque;
+use bevy::{ecs::component::Component, utils::default};
+use std::{collections::VecDeque, marker::PhantomData};
 
-#[derive(Component, PartialEq, Debug, Default)]
-pub struct Queue(VecDeque<Skill<Queued>>);
+#[derive(Debug, PartialEq)]
+pub struct DequeueAble;
 
-impl Enqueue<Skill<Queued>> for Queue {
+pub struct EnqueueAble;
+
+#[derive(Component, PartialEq, Debug)]
+pub struct Queue<TState = DequeueAble> {
+	queue: VecDeque<Skill<Queued>>,
+	state: PhantomData<TState>,
+}
+
+impl Default for Queue {
+	fn default() -> Self {
+		Self {
+			queue: default(),
+			state: default(),
+		}
+	}
+}
+
+impl Queue {
+	#[cfg(test)]
+	fn new<const N: usize>(queue: [Skill<Queued>; N]) -> Self {
+		Self {
+			queue: VecDeque::from(queue),
+			state: PhantomData,
+		}
+	}
+
+	pub fn to_enqueue_able(self) -> Queue<EnqueueAble> {
+		Queue {
+			queue: self.queue,
+			state: PhantomData,
+		}
+	}
+}
+
+impl Queue<EnqueueAble> {
+	pub fn to_dequeue_able(self) -> Queue<DequeueAble> {
+		Queue {
+			queue: self.queue,
+			state: PhantomData,
+		}
+	}
+}
+
+impl Enqueue<Skill<Queued>> for Queue<EnqueueAble> {
 	fn enqueue(&mut self, item: Skill<Queued>) {
-		self.0.push_back(item);
+		self.queue.push_back(item);
 	}
 }
 
 impl Dequeue<Skill<Queued>> for Queue {
 	fn dequeue(&mut self) -> Option<Skill<Queued>> {
-		self.0.pop_front()
+		self.queue.pop_front()
 	}
 }
 
-impl Iter<Skill<Queued>> for Queue {
+impl<TState> Iter<Skill<Queued>> for Queue<TState> {
 	fn iter<'a>(&'a self) -> impl DoubleEndedIterator<Item = &'a Skill<Queued>>
 	where
 		Skill<Queued>: 'a,
 	{
-		self.0.iter()
+		self.queue.iter()
 	}
 }
 
-impl IterMut<Skill<Queued>> for Queue {
+impl IterMut<Skill<Queued>> for Queue<EnqueueAble> {
 	fn iter_mut<'a>(&'a mut self) -> impl DoubleEndedIterator<Item = &'a mut Skill<Queued>>
 	where
 		Skill<Queued>: 'a,
 	{
-		self.0.iter_mut()
+		self.queue.iter_mut()
 	}
 }
 
@@ -47,7 +90,7 @@ mod tests {
 
 	#[test]
 	fn enqueue_one_skill() {
-		let mut queue = Queue::default();
+		let mut queue = Queue::default().to_enqueue_able();
 		queue.enqueue(Skill {
 			name: "my skill",
 			data: Queued(SlotKey::Hand(Side::Main)),
@@ -55,18 +98,18 @@ mod tests {
 		});
 
 		assert_eq!(
-			Queue(VecDeque::from([Skill {
+			Queue::new([Skill {
 				name: "my skill",
 				data: Queued(SlotKey::Hand(Side::Main)),
 				..default()
-			}])),
-			queue
+			}]),
+			queue.to_dequeue_able()
 		);
 	}
 
 	#[test]
 	fn enqueue_two_skills() {
-		let mut queue = Queue::default();
+		let mut queue = Queue::default().to_enqueue_able();
 		queue.enqueue(Skill {
 			name: "skill a",
 			data: Queued(SlotKey::Hand(Side::Off)),
@@ -79,7 +122,7 @@ mod tests {
 		});
 
 		assert_eq!(
-			Queue(VecDeque::from([
+			Queue::new([
 				Skill {
 					name: "skill a",
 					data: Queued(SlotKey::Hand(Side::Off)),
@@ -90,18 +133,18 @@ mod tests {
 					data: Queued(SlotKey::Hand(Side::Main)),
 					..default()
 				}
-			])),
-			queue
+			]),
+			queue.to_dequeue_able()
 		);
 	}
 
 	#[test]
 	fn dequeue_one_skill() {
-		let mut queue = Queue(VecDeque::from([Skill {
+		let mut queue = Queue::new([Skill {
 			name: "my skill",
 			data: Queued(SlotKey::Hand(Side::Main)),
 			..default()
-		}]));
+		}]);
 
 		assert_eq!(
 			(
@@ -118,7 +161,7 @@ mod tests {
 
 	#[test]
 	fn dequeue_two_skill() {
-		let mut queue = Queue(VecDeque::from([
+		let mut queue = Queue::new([
 			Skill {
 				name: "skill a",
 				data: Queued(SlotKey::Hand(Side::Off)),
@@ -129,7 +172,7 @@ mod tests {
 				data: Queued(SlotKey::Hand(Side::Main)),
 				..default()
 			},
-		]));
+		]);
 
 		assert_eq!(
 			(
@@ -153,7 +196,7 @@ mod tests {
 
 	#[test]
 	fn as_slice() {
-		let mut queue = Queue::default();
+		let mut queue = Queue::default().to_enqueue_able();
 		queue.enqueue(Skill {
 			name: "skill a",
 			data: Queued(SlotKey::Hand(Side::Off)),
@@ -184,7 +227,7 @@ mod tests {
 
 	#[test]
 	fn as_slice_mut() {
-		let mut queue = Queue::default();
+		let mut queue = Queue::default().to_enqueue_able();
 		queue.enqueue(Skill {
 			name: "skill a",
 			data: Queued(SlotKey::Hand(Side::Off)),
