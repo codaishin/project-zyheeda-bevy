4fd751ab88515aae1c9203f76b8164de0cf4c67a
codaishin <codaishin@gmail.com>
2024-05-03 17:20:30 +0200

    feat(animation): introduce more descriptive dispatch traits

--- src/plugins/animations/src/traits.rs
+++ src/plugins/animations/src/traits.rs
@@ -47,3 +47,151 @@ pub(crate) trait AnimationPlayMode {
 pub trait AnimationChainUpdate {
 	fn chain_update(&mut self, last: &Self);
 }
+
+pub struct SkillLayer;
+pub struct MovementLayer;
+pub struct IdleLayer;
+
+pub trait StartAnimation<TLayer, TAnimation> {
+	fn start_animation(&mut self, animation: TAnimation);
+}
+
+impl<T: InsertAnimation<TAnimation>, TAnimation> StartAnimation<SkillLayer, TAnimation> for T {
+	fn start_animation(&mut self, animation: TAnimation) {
+		self.insert(animation, Priority::High);
+	}
+}
+
+impl<T: InsertAnimation<TAnimation>, TAnimation> StartAnimation<MovementLayer, TAnimation> for T {
+	fn start_animation(&mut self, animation: TAnimation) {
+		self.insert(animation, Priority::Middle);
+	}
+}
+
+impl<T: InsertAnimation<TAnimation>, TAnimation> StartAnimation<IdleLayer, TAnimation> for T {
+	fn start_animation(&mut self, animation: TAnimation) {
+		self.insert(animation, Priority::Low);
+	}
+}
+
+pub trait StopAnimation<TLayer> {
+	fn stop_animation(&mut self);
+}
+
+impl<T: MarkObsolete> StopAnimation<SkillLayer> for T {
+	fn stop_animation(&mut self) {
+		self.mark_obsolete(Priority::High);
+	}
+}
+
+impl<T: MarkObsolete> StopAnimation<MovementLayer> for T {
+	fn stop_animation(&mut self) {
+		self.mark_obsolete(Priority::Middle);
+	}
+}
+
+impl<T: MarkObsolete> StopAnimation<IdleLayer> for T {
+	fn stop_animation(&mut self) {
+		self.mark_obsolete(Priority::Low);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use mockall::{mock, predicate::eq};
+
+	#[derive(Debug, PartialEq)]
+	struct _Animation(&'static str);
+
+	mock! {
+		_Dispatch {}
+		impl InsertAnimation<_Animation> for _Dispatch {
+			fn insert(&mut self, animation: _Animation, priority: Priority);
+		}
+		impl MarkObsolete for _Dispatch {
+			fn mark_obsolete(&mut self, priority: Priority);
+		}
+	}
+
+	#[test]
+	fn start_skill_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_insert()
+			.times(1)
+			.with(eq(_Animation("my animation")), eq(Priority::High))
+			.return_const(());
+
+		StartAnimation::<SkillLayer, _Animation>::start_animation(
+			dispatch,
+			_Animation("my animation"),
+		);
+	}
+
+	#[test]
+	fn start_movement_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_insert()
+			.times(1)
+			.with(eq(_Animation("my animation")), eq(Priority::Middle))
+			.return_const(());
+
+		StartAnimation::<MovementLayer, _Animation>::start_animation(
+			dispatch,
+			_Animation("my animation"),
+		);
+	}
+
+	#[test]
+	fn start_idle_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_insert()
+			.times(1)
+			.with(eq(_Animation("my animation")), eq(Priority::Low))
+			.return_const(());
+
+		StartAnimation::<IdleLayer, _Animation>::start_animation(
+			dispatch,
+			_Animation("my animation"),
+		);
+	}
+
+	#[test]
+	fn stop_skill_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_mark_obsolete()
+			.times(1)
+			.with(eq(Priority::High))
+			.return_const(());
+
+		StopAnimation::<SkillLayer>::stop_animation(dispatch);
+	}
+
+	#[test]
+	fn stop_movement_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_mark_obsolete()
+			.times(1)
+			.with(eq(Priority::Middle))
+			.return_const(());
+
+		StopAnimation::<MovementLayer>::stop_animation(dispatch);
+	}
+
+	#[test]
+	fn stop_idle_animation() {
+		let dispatch = &mut Mock_Dispatch::default();
+		dispatch
+			.expect_mark_obsolete()
+			.times(1)
+			.with(eq(Priority::Low))
+			.return_const(());
+
+		StopAnimation::<IdleLayer>::stop_animation(dispatch);
+	}
+}
