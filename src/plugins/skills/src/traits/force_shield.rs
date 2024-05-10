f989ddc3c5609e2e2a03e9f7b895169680fb428d
codaishin <codaishin@gmail.com>
2024-05-10 13:32:00 +0200

    feat(skills): impl `NewSkillBundle` for `ForceShield` behavior

---
+++ src/plugins/skills/src/traits/force_shield.rs
@@ -0,0 +1,87 @@
+use super::NewSkillBundle;
+use crate::skills::{SkillCaster, SkillSpawner, Target};
+use behaviors::components::ForceShield;
+use bevy::{self, ecs::bundle::Bundle, prelude::SpatialBundle, transform::components::Transform};
+
+impl NewSkillBundle for ForceShield {
+	fn new_bundle(caster: &SkillCaster, spawner: &SkillSpawner, _: &Target) -> impl Bundle {
+		(
+			ForceShield {
+				direction: caster.0.forward(),
+			},
+			SpatialBundle::from_transform(Transform::from(spawner.0)),
+		)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{skills::SelectInfo, test_tools::assert_spacial_bundle};
+	use bevy::{
+		app::App,
+		math::{Ray3d, Vec3},
+		transform::components::{GlobalTransform, Transform},
+	};
+	use common::assert_eq_approx;
+
+	fn target() -> Target {
+		SelectInfo {
+			ray: Ray3d::new(Vec3::ONE, Vec3::ONE),
+			collision_info: None,
+		}
+	}
+
+	#[test]
+	fn spawn_with_forward_direction() {
+		let mut app = App::new();
+		let forward = Vec3::new(8., 9., 10.);
+		let caster = SkillCaster(Transform::default().looking_at(forward, Vec3::Y));
+		let spawner = SkillSpawner(GlobalTransform::from_xyz(1., 2., 3.));
+
+		let force_shield = app
+			.world
+			.spawn(ForceShield::new_bundle(&caster, &spawner, &target()))
+			.id();
+		let force_shield = app.world.entity(force_shield).get::<ForceShield>();
+
+		assert_eq_approx!(
+			Some(forward.normalize()),
+			force_shield.map(|p| p.direction.into()),
+			0.0001
+		);
+	}
+
+	#[test]
+	fn spawn_with_special_bundle() {
+		let mut app = App::new();
+		let caster = SkillCaster::default();
+		let spawner = SkillSpawner(GlobalTransform::from_xyz(1., 2., 3.));
+
+		let force_shield = app
+			.world
+			.spawn(ForceShield::new_bundle(&caster, &spawner, &target()))
+			.id();
+		let force_shield = app.world.entity(force_shield);
+
+		assert_spacial_bundle!(force_shield);
+	}
+
+	#[test]
+	fn spawn_with_proper_location() {
+		let mut app = App::new();
+		let caster = SkillCaster::default();
+		let spawner = SkillSpawner(GlobalTransform::from_xyz(1., 2., 3.));
+
+		let force_shield = app
+			.world
+			.spawn(ForceShield::new_bundle(&caster, &spawner, &target()))
+			.id();
+		let force_shield = app.world.entity(force_shield).get::<Transform>();
+
+		assert_eq!(
+			Some(Vec3::new(1., 2., 3.)),
+			force_shield.map(|p| p.translation)
+		)
+	}
+}
