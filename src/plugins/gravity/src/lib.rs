ee236188a2c04bce186ad0163adc71dff6440a78
codaishin <codaishin@gmail.com>
2024-05-24 19:51:00 +0200

    feat(gravity): incorporate detection and application system

--- src/plugins/gravity/src/lib.rs
+++ src/plugins/gravity/src/lib.rs
@@ -1,7 +1,33 @@
-use bevy::app::{App, Plugin};
+pub mod components;
+pub mod traits;
+
+pub(crate) mod systems;
+
+use bevy::{
+	app::{App, Plugin, Update},
+	ecs::component::Component,
+};
+use bevy_rapier3d::geometry::CollidingEntities;
+use systems::{apply_gravity::apply_gravity, detect_gravity_effected::detect_gravity_effected};
+use traits::GetGravityPull;
 
 pub struct GravityPlugin;
 
 impl Plugin for GravityPlugin {
-	fn build(&self, _: &mut App) {}
+	fn build(&self, app: &mut App) {
+		app.add_systems(Update, apply_gravity);
+	}
+}
+
+pub trait AddGravityInteraction {
+	fn register_gravity_source<TGravitySource: Component + GetGravityPull>(&mut self);
+}
+
+impl AddGravityInteraction for App {
+	fn register_gravity_source<TGravitySource: Component + GetGravityPull>(&mut self) {
+		self.add_systems(
+			Update,
+			detect_gravity_effected::<CollidingEntities, TGravitySource>,
+		);
+	}
 }
