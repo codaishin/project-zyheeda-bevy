9850006f30ee755a4aaa0a7a49ad36a270f6c702
Codaishin <Codaishin@gmail.com>
2024-06-05 12:24:52 +0200

    chore(ingame_menu): improve structure for readability

--- src/plugins/ingame_menu/src/lib.rs
+++ src/plugins/ingame_menu/src/lib.rs
@@ -49,51 +49,63 @@ pub struct IngameMenuPlugin;
 
 impl Plugin for IngameMenuPlugin {
 	fn build(&self, app: &mut App) {
-		app.init_state::<MenuState>()
-			.init_resource::<Shared<Path, Icon>>()
-			.add_systems(Update, set_state_from_input::<MenuState>)
-			.add_systems(
-				OnEnter(MenuState::Inventory),
-				(spawn::<InventoryScreen>, set_state::<GameRunning, Off>),
-			)
-			.add_systems(
-				OnExit(MenuState::Inventory),
-				(despawn::<InventoryScreen>, set_state::<GameRunning, On>),
-			)
-			.add_systems(
-				Update,
-				(
-					panel_colors::<InventoryPanel>,
-					panel_container_states::<InventoryPanel, InventoryKey, Inventory>,
-					panel_container_states::<InventoryPanel, SlotKey, Slots>,
-					drag::<Player, InventoryKey>,
-					drag::<Player, SlotKey>,
-					drop::<Player, InventoryKey, InventoryKey>,
-					drop::<Player, SlotKey, SlotKey>,
-					drop::<Player, SlotKey, InventoryKey>,
-					drop::<Player, InventoryKey, SlotKey>,
-				)
-					.run_if(in_state(MenuState::Inventory)),
-			)
-			.add_systems(
-				Update,
-				(swap_equipped_items.pipe(log_many), swap_inventory_items),
-			)
-			.add_systems(OnEnter(MenuState::None), spawn::<UIOverlay>)
-			.add_systems(OnExit(MenuState::None), despawn::<UIOverlay>)
-			.add_systems(
-				Update,
-				(
-					quickbar::<Queue, Combos, ComboLinger, AssetServer>,
-					update_label_text::<QuickbarPanel>,
-					panel_colors::<QuickbarPanel>,
-					panel_activity_colors_override::<Queue, QuickbarPanel>,
-				)
-					.run_if(in_state(MenuState::None)),
-			)
-			.add_systems(
-				Update,
-				(set_ui_mouse_context, prime_mouse_context::<QuickbarPanel>),
-			);
+		resources(app);
+		state_control_systems(app);
+		ui_overlay_systems(app);
+		inventory_screen_systems(app);
 	}
+}
+
+fn resources(app: &mut App) {
+	app.init_state::<MenuState>()
+		.init_resource::<Shared<Path, Icon>>();
+}
+
+fn state_control_systems(app: &mut App) {
+	app.add_systems(Update, set_state_from_input::<MenuState>)
+		.add_systems(OnExit(MenuState::None), set_state::<GameRunning, Off>)
+		.add_systems(OnEnter(MenuState::None), set_state::<GameRunning, On>);
+}
+
+fn ui_overlay_systems(app: &mut App) {
+	app.add_systems(OnEnter(MenuState::None), spawn::<UIOverlay>)
+		.add_systems(OnExit(MenuState::None), despawn::<UIOverlay>)
+		.add_systems(
+			Update,
+			(
+				quickbar::<Queue, Combos, ComboLinger, AssetServer>,
+				update_label_text::<QuickbarPanel>,
+				panel_colors::<QuickbarPanel>,
+				panel_activity_colors_override::<Queue, QuickbarPanel>,
+			)
+				.run_if(in_state(MenuState::None)),
+		)
+		.add_systems(
+			Update,
+			(set_ui_mouse_context, prime_mouse_context::<QuickbarPanel>),
+		);
+}
+
+fn inventory_screen_systems(app: &mut App) {
+	app.add_systems(OnEnter(MenuState::Inventory), spawn::<InventoryScreen>)
+		.add_systems(OnExit(MenuState::Inventory), despawn::<InventoryScreen>)
+		.add_systems(
+			Update,
+			(
+				panel_colors::<InventoryPanel>,
+				panel_container_states::<InventoryPanel, InventoryKey, Inventory>,
+				panel_container_states::<InventoryPanel, SlotKey, Slots>,
+				drag::<Player, InventoryKey>,
+				drag::<Player, SlotKey>,
+				drop::<Player, InventoryKey, InventoryKey>,
+				drop::<Player, SlotKey, SlotKey>,
+				drop::<Player, SlotKey, InventoryKey>,
+				drop::<Player, InventoryKey, SlotKey>,
+			)
+				.run_if(in_state(MenuState::Inventory)),
+		)
+		.add_systems(
+			Update,
+			(swap_equipped_items.pipe(log_many), swap_inventory_items),
+		);
 }
