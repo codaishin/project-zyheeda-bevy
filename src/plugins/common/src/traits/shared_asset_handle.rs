0edef9ead421aee31c67b1ca7edaf4e14dde9021
Codaishin <Codaishin@gmail.com>
2024-06-07 15:29:53 +0200

    feat(common): add `SharedAssetHandleProcedural` trait

--- src/plugins/common/src/traits/shared_asset_handle.rs
+++ src/plugins/common/src/traits/shared_asset_handle.rs
@@ -5,3 +5,12 @@ use bevy::asset::{Asset, Handle};
 pub trait SharedAssetHandle<TCache, TKey, TAsset: Asset> {
 	fn handle(&mut self, cache: &mut TCache, key: TKey) -> Handle<TAsset>;
 }
+
+pub trait SharedAssetHandleProcedural<TCache, TKey, TAsset: Asset> {
+	fn handle(
+		&mut self,
+		cache: &mut TCache,
+		key: TKey,
+		new: impl FnOnce() -> TAsset,
+	) -> Handle<TAsset>;
+}
