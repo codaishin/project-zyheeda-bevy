45ba7188454474e0dad0715f8536e75ec231f2d5
Codaishin <Codaishin@gmail.com>
2024-06-10 13:56:01 +0200

    feat(common): load external assets via asset server and cache tuple

--- src/plugins/common/src/traits/cache.rs
+++ src/plugins/common/src/traits/cache.rs
@@ -1,3 +1,12 @@
+pub mod get_or_load_asset;
+
+use super::load_asset::Path;
+use bevy::asset::{Asset, Handle};
+
 pub trait Cache<TKey, TValue> {
 	fn cached(&mut self, key: TKey, new: impl FnOnce() -> TValue) -> TValue;
+}
+
+pub trait GetOrLoadAsset<TAsset: Asset> {
+	fn get_or_load(&mut self, key: Path) -> Handle<TAsset>;
 }
---
+++ src/plugins/common/src/traits/cache/get_or_load_asset.rs
@@ -0,0 +1,134 @@
+use super::{Cache, GetOrLoadAsset};
+use crate::traits::load_asset::{LoadAsset, Path};
+use bevy::{
+	asset::{Asset, Handle},
+	prelude::{Res, ResMut, Resource},
+};
+
+impl<TAssets, TAsset, TCache> GetOrLoadAsset<TAsset> for (Res<'_, TAssets>, ResMut<'_, TCache>)
+where
+	TAssets: Resource + LoadAsset<TAsset>,
+	TAsset: Asset,
+	TCache: Resource + Cache<Path, Handle<TAsset>>,
+{
+	fn get_or_load(&mut self, key: Path) -> Handle<TAsset> {
+		let (assets, cache) = self;
+		cache.cached(key.clone(), || assets.load_asset(key))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::test_tools::utils::SingleThreadedApp;
+	use bevy::{
+		app::{App, Update},
+		asset::AssetId,
+		prelude::default,
+		render::texture::Image,
+		utils::Uuid,
+	};
+	use std::{cell::RefCell, sync::Mutex};
+
+	#[derive(Default, Resource)]
+	struct _Cache {
+		args: Vec<(Path, Handle<Image>)>,
+		returns: Handle<Image>,
+	}
+
+	impl Cache<Path, Handle<Image>> for _Cache {
+		fn cached(&mut self, key: Path, new: impl FnOnce() -> Handle<Image>) -> Handle<Image> {
+			self.args.push((key, new()));
+			self.returns.clone()
+		}
+	}
+
+	#[derive(Default, Resource)]
+	struct _Assets {
+		args: Mutex<RefCell<Vec<Path>>>,
+		returns: Handle<Image>,
+	}
+
+	impl LoadAsset<Image> for _Assets {
+		fn load_asset(&self, path: Path) -> Handle<Image> {
+			let Ok(args) = self.args.lock() else {
+				panic!("Can't acquire args lock in _Assets");
+			};
+
+			args.borrow_mut().push(path);
+			self.returns.clone()
+		}
+	}
+
+	fn setup() -> App {
+		let mut app = App::new().single_threaded(Update);
+		app.init_resource::<_Assets>();
+		app.init_resource::<_Cache>();
+
+		app
+	}
+
+	fn run_system(
+		app: &mut App,
+		mut callback: impl FnMut(Res<_Assets>, ResMut<_Cache>) + Send + Sync + 'static,
+	) {
+		app.add_systems(
+			Update,
+			move |assets: Res<_Assets>, cache: ResMut<_Cache>| {
+				callback(assets, cache);
+			},
+		);
+		app.update();
+	}
+
+	#[test]
+	fn return_cached_asset() {
+		let cached_asset = Handle::Weak(AssetId::Uuid {
+			uuid: Uuid::new_v4(),
+		});
+		let mut app = setup();
+
+		app.insert_resource(_Cache {
+			returns: cached_asset.clone(),
+			..default()
+		});
+
+		run_system(&mut app, move |assets, cache| {
+			let handle = (assets, cache).get_or_load(Path::from(""));
+			assert_eq!(cached_asset, handle);
+		})
+	}
+
+	#[test]
+	fn call_cached_with_proper_args() {
+		let handle = Handle::Weak(AssetId::Uuid {
+			uuid: Uuid::new_v4(),
+		});
+		let mut app = setup();
+
+		app.insert_resource(_Assets {
+			returns: handle.clone(),
+			..default()
+		});
+
+		run_system(&mut app, |assets, cache| {
+			(assets, cache).get_or_load(Path::from("proper path"));
+		});
+
+		let cache = app.world.resource::<_Cache>();
+		assert_eq!(vec![(Path::from("proper path"), handle)], cache.args);
+	}
+
+	#[test]
+	fn call_load_asset_with_proper_path() {
+		let mut app = setup();
+
+		run_system(&mut app, |assets, cache| {
+			(assets, cache).get_or_load(Path::from("proper path"));
+		});
+
+		let assets = app.world.resource::<_Assets>();
+		let args = assets.args.lock().unwrap();
+		assert_eq!(vec![Path::from("proper path")], *args.borrow());
+	}
+}
