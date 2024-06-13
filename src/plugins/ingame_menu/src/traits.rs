7c14b2698cf12792c0e266a6783228e68ac8e93a
Codaishin <Codaishin@gmail.com>
2024-06-13 13:18:07 +0200

    feat(ingame_menu): add `InsertCombo` trait

--- src/plugins/ingame_menu/src/traits.rs
+++ src/plugins/ingame_menu/src/traits.rs
@@ -2,3 +2,20 @@ pub mod children;
 pub mod colors;
 pub mod get_style;
 pub mod set;
+
+use bevy::{asset::Handle, ecs::system::EntityCommands, render::texture::Image};
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct SkillDescriptor<TKey> {
+	pub name: &'static str,
+	pub key: TKey,
+	pub icon: Option<Handle<Image>>,
+}
+
+pub(crate) trait InsertCombo<TKey> {
+	fn insert_combo<'a>(
+		&'a mut self,
+		entity: &'a mut EntityCommands<'a>,
+		combo: Vec<SkillDescriptor<TKey>>,
+	);
+}
