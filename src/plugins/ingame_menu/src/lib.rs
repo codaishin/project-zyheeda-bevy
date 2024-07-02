084d4f74e70a688456aae2f412c2896eb1de932f
Codaishin <Codaishin@gmail.com>
2024-07-02 13:49:28 +0200

    feat(ingame_menU): add `dropdown` system

--- src/plugins/ingame_menu/src/lib.rs
+++ src/plugins/ingame_menu/src/lib.rs
@@ -6,8 +6,6 @@ mod traits;
 #[cfg(debug_assertions)]
 mod debug;
 
-use std::time::Duration;
-
 use bevy::prelude::*;
 use common::{
 	components::Player,
@@ -35,6 +33,7 @@ use skills::{
 	},
 	items::{inventory_key::InventoryKey, slot_key::SlotKey},
 };
+use std::time::Duration;
 use systems::{
 	added::added,
 	combos::{
@@ -44,6 +43,7 @@ use systems::{
 	},
 	dad::{drag::drag, drop::drop},
 	despawn::despawn,
+	dropdown::dropdown,
 	items::swap::{equipped_items::swap_equipped_items, inventory_items::swap_inventory_items},
 	mouse_context::{prime::prime_mouse_context, set_ui::set_ui_mouse_context},
 	set_state::set_state,
@@ -110,6 +110,7 @@ impl Plugin for IngameMenuPlugin {
 		combo_overview_systems(app);
 		inventory_screen_systems(app);
 		tooltip_systems(app);
+		dropdown_systems(app);
 
 		#[cfg(debug_assertions)]
 		debug::setup_run_time_display(app);
@@ -205,4 +206,8 @@ fn inventory_screen_systems(app: &mut App) {
 
 fn tooltip_systems(app: &mut App) {
 	app.add_systems(Update, tooltip_visibility::<Real>);
+}
+
+fn dropdown_systems(app: &mut App) {
+	app.add_systems(Update, dropdown);
 }
