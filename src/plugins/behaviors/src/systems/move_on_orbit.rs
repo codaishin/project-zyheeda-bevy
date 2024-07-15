37be08203628f275a859abcbab862987e3df95d4
Codaishin <Codaishin@gmail.com>
2024-07-15 18:23:13 +0200

    chore(behaviors): simplify `move_on_orbit`
    `MouseMotion` events not seem to work as expected. Code will be simplified and explanatory comments removed.

--- src/plugins/behaviors/src/systems/move_on_orbit.rs
+++ src/plugins/behaviors/src/systems/move_on_orbit.rs
@@ -7,11 +7,6 @@ pub(crate) fn move_on_orbit<TOrbitComponent: Orbit + Component>(
 	mut query: Query<(&TOrbitComponent, &mut Transform)>,
 ) {
 	if !mouse.pressed(MouseButton::Right) {
-		// It seems odd to do this, but after moving this into a plugin we observe
-		// events to persist more than 2 frames, so we clear manually.
-		// This seems to be at odds with the documentation at https://docs.rs/bevy/0.12.1/bevy/prelude/struct.Events.html
-		// but in line with comments in https://github.com/bevyengine/bevy/issues/10860.
-		mouse_motion.clear();
 		return;
 	}
 
@@ -57,10 +52,7 @@ mod tests {
 		let input = ButtonInput::<MouseButton>::default();
 
 		app.add_systems(Update, move_on_orbit::<_Orbit>);
-		// Not using `add_event`, because we don't want events to be cleared between frames
-		// so we can simulate the observed behavior of events not being cleared when running
-		// the plugin within our game.
-		app.init_resource::<Events<MouseMotion>>();
+		app.add_event::<MouseMotion>();
 		app.insert_resource(input);
 
 		app
@@ -109,43 +101,6 @@ mod tests {
 		app.world_mut()
 			.resource_mut::<Events<MouseMotion>>()
 			.send(MouseMotion { delta: angels });
-
-		app.update();
-	}
-
-	#[test]
-	fn disregard_events_that_happened_when_not_right_mouse_pressed() {
-		let mut app = setup_app();
-		let mut orbit = _Orbit::new();
-		let agent = Transform::from_translation(Vec3::ZERO);
-		let angels = Vec2::new(3., 4.);
-		let discard_angles = Vec2::new(100., 200.);
-
-		orbit
-			.mock
-			.expect_orbit()
-			.with(eq(agent), eq(angels))
-			.times(1)
-			.return_const(());
-
-		app.world_mut().spawn((orbit, agent));
-
-		app.update();
-
-		app.world_mut()
-			.resource_mut::<Events<MouseMotion>>()
-			.send(MouseMotion {
-				delta: discard_angles,
-			});
-
-		app.update();
-
-		app.world_mut()
-			.resource_mut::<Events<MouseMotion>>()
-			.send(MouseMotion { delta: angels });
-		app.world_mut()
-			.resource_mut::<ButtonInput<MouseButton>>()
-			.press(MouseButton::Right);
 
 		app.update();
 	}
