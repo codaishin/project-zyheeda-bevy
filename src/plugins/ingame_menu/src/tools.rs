cb043566e4e9560040ac9d77cc21623b755f28ba
Codaishin <Codaishin@gmail.com>
2024-08-09 15:12:31 +0200

    feat(ingame_menu): `Pixel` math

--- src/plugins/ingame_menu/src/tools.rs
+++ src/plugins/ingame_menu/src/tools.rs
@@ -1,5 +1,7 @@
 pub(crate) mod menu_state;
 
+use std::ops::{Add, Div, Mul, Sub};
+
 use bevy::ui::{UiRect, Val};
 use common::tools::Index;
 
@@ -25,7 +27,7 @@ impl Default for Layout {
 	}
 }
 
-#[derive(Default)]
+#[derive(Default, Debug, PartialEq)]
 pub(crate) struct Pixel(pub f32);
 
 impl From<Pixel> for Val {
@@ -37,6 +39,38 @@ impl From<Pixel> for Val {
 impl From<Pixel> for UiRect {
 	fn from(value: Pixel) -> Self {
 		UiRect::all(Val::from(value))
+	}
+}
+
+impl Sub for Pixel {
+	type Output = Pixel;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		Pixel(self.0 - rhs.0)
+	}
+}
+
+impl Add for Pixel {
+	type Output = Pixel;
+
+	fn add(self, rhs: Self) -> Self::Output {
+		Pixel(self.0 + rhs.0)
+	}
+}
+
+impl Div<f32> for Pixel {
+	type Output = Pixel;
+
+	fn div(self, rhs: f32) -> Self::Output {
+		Pixel(self.0 / rhs)
+	}
+}
+
+impl Mul<f32> for Pixel {
+	type Output = Pixel;
+
+	fn mul(self, rhs: f32) -> Self::Output {
+		Pixel(self.0 * rhs)
 	}
 }
 
@@ -58,5 +92,66 @@ impl Dimensions {
 
 	pub(crate) fn nested_minimum(&self) -> Pixel {
 		Pixel(-self.border.0)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn pixel_42_sub_10() {
+		let pixel = Pixel(42.);
+
+		assert_eq!(Pixel(10.), pixel - Pixel(32.))
+	}
+
+	#[test]
+	fn pixel_11_sub_6() {
+		let pixel = Pixel(11.);
+
+		assert_eq!(Pixel(5.), pixel - Pixel(6.))
+	}
+
+	#[test]
+	fn pixel_42_add_10() {
+		let pixel = Pixel(42.);
+
+		assert_eq!(Pixel(74.), pixel + Pixel(32.))
+	}
+
+	#[test]
+	fn pixel_11_add_6() {
+		let pixel = Pixel(11.);
+
+		assert_eq!(Pixel(17.), pixel + Pixel(6.))
+	}
+
+	#[test]
+	fn pixel_42_div_10() {
+		let pixel = Pixel(42.);
+
+		assert_eq!(Pixel(42. / 32.), pixel / 32.)
+	}
+
+	#[test]
+	fn pixel_11_div_6() {
+		let pixel = Pixel(11.);
+
+		assert_eq!(Pixel(11. / 6.), pixel / 6.)
+	}
+
+	#[test]
+	fn pixel_42_mul_10() {
+		let pixel = Pixel(42.);
+
+		assert_eq!(Pixel(42. * 32.), pixel * 32.)
+	}
+
+	#[test]
+	fn pixel_11_mul_6() {
+		let pixel = Pixel(11.);
+
+		assert_eq!(Pixel(11. * 6.), pixel * 6.)
 	}
 }
