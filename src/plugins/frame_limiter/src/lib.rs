7079d03c7571eacc0275fbc4a5b611aa8276c21c
Codaishin <Codaishin@gmail.com>
2024-12-18 12:44:57 +0100

    feat(frame_limiter): sleep 1/60 second in render cleanup

--- src/plugins/frame_limiter/src/lib.rs
+++ src/plugins/frame_limiter/src/lib.rs
@@ -1,7 +1,29 @@
-use bevy::prelude::*;
+use bevy::{
+	prelude::*,
+	render::{Render, RenderApp, RenderSet},
+};
+use std::{
+	thread,
+	time::Duration,
+};
 
 pub struct FrameLimiterPlugin;
 
 impl Plugin for FrameLimiterPlugin {
-	fn build(&self, _: &mut App) {}
+	fn build(&self, app: &mut App) {
+		app.sub_app_mut(RenderApp)
+			.insert_resource(Sleep(Duration::from_secs(1) / 60))
+			.add_systems(Render, Sleep::system.in_set(RenderSet::Cleanup));
+	}
+}
+
+#[derive(Resource, Debug, PartialEq)]
+struct Sleep(Duration);
+
+impl Sleep {
+	fn system(sleep: Res<Sleep>) {
+		let Sleep(sleep) = *sleep;
+
+		thread::sleep(sleep);
+	}
 }
