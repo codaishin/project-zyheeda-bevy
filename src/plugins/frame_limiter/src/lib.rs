5474cdf555e0e9a3ef088232af83648700132c51
Codaishin <Codaishin@gmail.com>
2024-12-18 14:06:42 +0100

    feat(frame_limiter): debug print fps

--- src/plugins/frame_limiter/src/lib.rs
+++ src/plugins/frame_limiter/src/lib.rs
@@ -15,6 +15,9 @@ impl Plugin for FrameLimiterPlugin {
 			.insert_resource(LastSleep(Instant::now()))
 			.insert_resource(Sleep(Duration::from_secs(1) / 60))
 			.add_systems(Render, Sleep::system.in_set(RenderSet::Cleanup));
+
+		#[cfg(debug_assertions)]
+		app.add_systems(Update, debug::MeasureFps::system);
 	}
 }
 
@@ -32,5 +35,44 @@ impl Sleep {
 
 		thread::sleep(sleep);
 		*last_sleep = Instant::now();
+	}
+}
+
+#[cfg(debug_assertions)]
+mod debug {
+	use bevy::prelude::Local;
+	use std::{ops::DerefMut, time::Instant};
+
+	#[derive(Debug, PartialEq)]
+	pub(super) struct MeasureFps {
+		timer: Instant,
+		counter: u32,
+		fps: u32,
+	}
+
+	impl Default for MeasureFps {
+		fn default() -> Self {
+			Self {
+				timer: Instant::now(),
+				counter: 0,
+				fps: u32::MAX,
+			}
+		}
+	}
+
+	impl MeasureFps {
+		pub(super) fn system(mut fps: Local<MeasureFps>) {
+			let fps = fps.deref_mut();
+			fps.counter += 1;
+
+			if fps.timer.elapsed().as_secs() < 1 {
+				return;
+			}
+
+			fps.fps = fps.counter;
+			fps.timer = Instant::now();
+			fps.counter = 0;
+			println!("FPS: {}", fps.fps);
+		}
 	}
 }
