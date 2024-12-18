78722d7899776ba9871cdf671c6b0e45ba48d8ed
Codaishin <Codaishin@gmail.com>
2024-12-18 13:35:30 +0100

    feat(frame_limiter): adjust sleep for already elapsed time

--- src/plugins/frame_limiter/src/lib.rs
+++ src/plugins/frame_limiter/src/lib.rs
@@ -4,7 +4,7 @@ use bevy::{
 };
 use std::{
 	thread,
-	time::Duration,
+	time::{Duration, Instant},
 };
 
 pub struct FrameLimiterPlugin;
@@ -12,18 +12,25 @@ pub struct FrameLimiterPlugin;
 impl Plugin for FrameLimiterPlugin {
 	fn build(&self, app: &mut App) {
 		app.sub_app_mut(RenderApp)
+			.insert_resource(LastSleep(Instant::now()))
 			.insert_resource(Sleep(Duration::from_secs(1) / 60))
 			.add_systems(Render, Sleep::system.in_set(RenderSet::Cleanup));
 	}
 }
 
 #[derive(Resource, Debug, PartialEq)]
+struct LastSleep(Instant);
+
+#[derive(Resource, Debug, PartialEq)]
 struct Sleep(Duration);
 
 impl Sleep {
-	fn system(sleep: Res<Sleep>) {
+	fn system(sleep: Res<Sleep>, mut last_sleep: ResMut<LastSleep>) {
 		let Sleep(sleep) = *sleep;
+		let LastSleep(last_sleep) = last_sleep.as_mut();
+		let sleep = sleep.saturating_sub(last_sleep.elapsed());
 
 		thread::sleep(sleep);
+		*last_sleep = Instant::now();
 	}
 }
