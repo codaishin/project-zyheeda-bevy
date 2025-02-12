a08770394c0b69924ff53ec790ba561331ee6ffb
Codaishin <codaishin@gmail.com>
2025-02-12 17:24:00 +0100

    feat(skills): simplify `ModelRender`

--- src/plugins/skills/src/components/model_render.rs
+++ src/plugins/skills/src/components/model_render.rs
@@ -1,10 +1,9 @@
-use common::components::AssetModel;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Default, Clone, Serialize, Deserialize)]
 pub enum ModelRender {
 	#[default]
 	None,
-	Hand(AssetModel),
-	Forearm(AssetModel),
+	Hand(String),
+	Forearm(String),
 }
