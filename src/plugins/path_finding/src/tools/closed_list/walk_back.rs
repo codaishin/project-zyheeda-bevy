a66ad323b424946f0c43bcdab5823c1b39098772
Codaishin <codaishin@gmail.com>
2025-02-26 18:42:46 +0100

    feat(path_finding): `WalkBack` omits start

--- src/plugins/path_finding/src/tools/closed_list/walk_back.rs
+++ src/plugins/path_finding/src/tools/closed_list/walk_back.rs
@@ -23,23 +23,17 @@ impl ClosedList {
 	}
 }
 
-impl WalkBack {
-	fn parent(&self, node: &NavGridNode) -> Option<&NavGridNode> {
-		if node == self.list.start() {
-			return None;
-		}
-
-		self.list.parent(node)
-	}
-}
-
 impl Iterator for WalkBack {
 	type Item = NavGridNode;
 
 	fn next(&mut self) -> Option<Self::Item> {
 		let current = self.next?;
 
-		self.next = self.parent(&current).copied();
+		if &current == self.list.start() {
+			return None;
+		}
+
+		self.next = self.list.parent(&current).copied();
 
 		Some(current)
 	}
@@ -50,7 +44,7 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn iterate_backwards() {
+	fn iterate_backwards_omitting_start() {
 		let a = NavGridNode { x: 1, y: 2 };
 		let b = NavGridNode { x: 2, y: 2 };
 		let c = NavGridNode { x: 3, y: 2 };
@@ -61,6 +55,6 @@ mod tests {
 
 		let nodes = path.collect::<Vec<_>>();
 
-		assert_eq!(vec![c, b, a], nodes);
+		assert_eq!(vec![c, b], nodes);
 	}
 }
