bc5089666d6ed989669a7dd62f7ad78aae3d6156
Codaishin <codaishin@gmail.com>
2025-05-08 13:33:59 +0200

    feat(camera): inject settings plugin

--- src/plugins/camera_control/src/lib.rs
+++ src/plugins/camera_control/src/lib.rs
@@ -8,6 +8,7 @@ use common::{
 	traits::{
 		handles_graphics::{FirstPassCamera, WorldCameras},
 		handles_player::{HandlesPlayer, PlayerMainCamera},
+		handles_settings::HandlesSettings,
 		thread_safe::ThreadSafe,
 	},
 };
@@ -21,18 +22,21 @@ use systems::{
 
 pub struct CameraControlPlugin<TDependencies>(PhantomData<TDependencies>);
 
-impl<TPlayers, TGraphics> CameraControlPlugin<(TPlayers, TGraphics)>
+impl<TSettings, TPlayers, TGraphics> CameraControlPlugin<(TSettings, TPlayers, TGraphics)>
 where
+	TSettings: ThreadSafe + HandlesSettings,
 	TPlayers: ThreadSafe + HandlesPlayer + PlayerMainCamera,
 	TGraphics: ThreadSafe + WorldCameras + FirstPassCamera,
 {
-	pub fn depends_on(_: &TPlayers, _: &TGraphics) -> Self {
+	pub fn depends_on(_: &TSettings, _: &TPlayers, _: &TGraphics) -> Self {
 		Self(PhantomData)
 	}
 }
 
-impl<TPlayers, TGraphics> Plugin for CameraControlPlugin<(TPlayers, TGraphics)>
+impl<TSettings, TPlayers, TGraphics> Plugin
+	for CameraControlPlugin<(TSettings, TPlayers, TGraphics)>
 where
+	TSettings: ThreadSafe + HandlesSettings,
 	TPlayers: ThreadSafe + HandlesPlayer + PlayerMainCamera,
 	TGraphics: ThreadSafe + WorldCameras + FirstPassCamera,
 {
