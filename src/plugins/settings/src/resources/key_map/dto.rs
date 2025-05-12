4e28fbc04a89ff87f4f4e1b38517104415e4fce9
Codaishin <codaishin@gmail.com>
2025-05-12 11:41:52 +0200

    feat(settings): `KeyMapDto` use `IterFinite` trait for consistent key ordering

--- src/plugins/settings/src/resources/key_map/dto.rs
+++ src/plugins/settings/src/resources/key_map/dto.rs
@@ -1,7 +1,7 @@
 use super::KeyMap;
 use common::{
 	tools::action_key::{ActionKey, user_input::UserInput},
-	traits::handles_custom_assets::AssetFileExtensions,
+	traits::{handles_custom_assets::AssetFileExtensions, iteration::IterFinite},
 };
 use serde::{Deserialize, Serialize};
 use std::hash::Hash;
@@ -37,7 +37,64 @@ impl AssetFileExtensions for KeyMapDto<ActionKey, UserInput> {
 impl From<KeyMap> for KeyMapDto<ActionKey, UserInput> {
 	fn from(KeyMap(map): KeyMap) -> Self {
 		Self {
-			actions: Vec::from_iter(map.action_to_input),
+			actions: ActionKey::iterator()
+				.filter_map(|action| Some((action, *map.action_to_input.get(&action)?)))
+				.collect::<Vec<_>>(),
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::resources::key_map::KeyMapInternal;
+	use bevy::input::keyboard::KeyCode;
+	use common::{
+		repeat_scope,
+		tools::action_key::camera_key::CameraKey,
+		traits::handles_settings::UpdateKey,
+	};
+
+	#[test]
+	fn convert_to_dto_with_key_standard_ordering() {
+		repeat_scope!(10, {
+			let map = KeyMap(KeyMapInternal::default());
+
+			let dto = KeyMapDto::from(map);
+
+			assert_eq!(
+				ActionKey::iterator()
+					.map(|action| (action, UserInput::from(action)))
+					.collect::<Vec<_>>(),
+				dto.actions,
+			);
+		});
+	}
+
+	#[test]
+	fn convert_to_dto_with_key_standard_ordering_with_overrides() {
+		repeat_scope!(10, {
+			let mut map = KeyMap(KeyMapInternal::default());
+			map.update_key(
+				ActionKey::Camera(CameraKey::Rotate),
+				UserInput::KeyCode(KeyCode::F35),
+			);
+
+			let dto = KeyMapDto::from(map);
+
+			assert_eq!(
+				ActionKey::iterator()
+					.map(|action| (
+						action,
+						if action != ActionKey::Camera(CameraKey::Rotate) {
+							UserInput::from(action)
+						} else {
+							UserInput::KeyCode(KeyCode::F35)
+						}
+					))
+					.collect::<Vec<_>>(),
+				dto.actions,
+			);
+		});
 	}
 }
