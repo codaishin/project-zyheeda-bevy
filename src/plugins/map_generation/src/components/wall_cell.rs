48cd4d4b7d1dd662e1610a2de91ed12ce7863f8d
Codaishin <codaishin@gmail.com>
2025-06-11 12:57:32 +0200

    feat(map_generation): use lazy lock cube for `WallCell` collider

--- src/plugins/map_generation/src/components/wall_cell.rs
+++ src/plugins/map_generation/src/components/wall_cell.rs
@@ -4,12 +4,15 @@ use common::{
 	blocker::{Blocker, Blockers},
 	components::no_target::NoTarget,
 };
+use std::sync::LazyLock;
 
 #[derive(Component, Debug, PartialEq)]
 #[require(
 	Transform,
 	Blockers = [Blocker::Physical],
-	Collider = Collider::cuboid(0.5, 0.5, 0.5),
+	Collider = UNIT_CUBE.clone(),
 	NoTarget
 )]
 pub(crate) struct WallCell;
+
+static UNIT_CUBE: LazyLock<Collider> = LazyLock::new(|| Collider::cuboid(0.5, 0.5, 0.5));
