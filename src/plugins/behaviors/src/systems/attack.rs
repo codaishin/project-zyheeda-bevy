df0c334bd68470d093bc7d816edc7fde654ecca1
Codaishin <codaishin@gmail.com>
2025-07-30 15:07:54 +0200

    feat(behaviors): properly nest attack under attacker

--- src/plugins/behaviors/src/systems/attack.rs
+++ src/plugins/behaviors/src/systems/attack.rs
@@ -1,7 +1,7 @@
 use crate::components::{Attack, on_cool_down::OnCoolDown};
 use bevy::prelude::*;
 use common::{
-	components::persistent_entity::PersistentEntity,
+	components::{ground_offset::GroundOffset, persistent_entity::PersistentEntity},
 	traits::{
 		handles_enemies::{Attacker, EnemyAttack, Target},
 		try_despawn::TryDespawn,
@@ -12,17 +12,30 @@ use common::{
 
 impl<T> AttackSystem for T {}
 
+type AttackerComponents<'a, TAttacker> = (
+	Entity,
+	&'a PersistentEntity,
+	&'a TAttacker,
+	&'a Attack,
+	Option<&'a GroundOffset>,
+);
+
 pub trait AttackSystem {
 	fn attack(
 		mut commands: Commands,
 		mut stop_attacks: RemovedComponents<Attack>,
-		attackers: Query<(Entity, &PersistentEntity, &Self, &Attack), Without<OnCoolDown>>,
+		attackers: Query<AttackerComponents<Self>, Without<OnCoolDown>>,
 		ongoing: Query<&Ongoing>,
 	) where
 		Self: Component + EnemyAttack + Sized,
 	{
-		for (entity, persistent_entity, enemy, Attack(target)) in &attackers {
-			let mut attack_entity = commands.spawn_empty();
+		for (entity, persistent_entity, enemy, Attack(target), offset) in &attackers {
+			let offset = match offset {
+				Some(GroundOffset(offset)) => *offset,
+				None => Vec3::ZERO,
+			};
+			let mut attack_entity =
+				commands.spawn((ChildOf(entity), Transform::from_translation(offset)));
 			let attack = attack_entity.id();
 
 			enemy.insert_attack(
@@ -30,9 +43,7 @@ pub trait AttackSystem {
 				Attacker(*persistent_entity),
 				Target(*target),
 			);
-			attack_entity
-				.commands()
-				.try_insert_on(entity, (OnCoolDown(enemy.cool_down()), Ongoing(attack)));
+			commands.try_insert_on(entity, (OnCoolDown(enemy.cool_down()), Ongoing(attack)));
 		}
 
 		for attacker in stop_attacks.read() {
@@ -60,7 +71,7 @@ mod tests {
 	use macros::NestedMocks;
 	use mockall::automock;
 	use std::time::Duration;
-	use testing::{NestedMocks, SingleThreadedApp};
+	use testing::{NestedMocks, SingleThreadedApp, assert_count, get_children};
 
 	#[derive(Component, Debug, PartialEq)]
 	struct _FakeAttack {
@@ -105,36 +116,78 @@ mod tests {
 		let mut app = setup();
 		let target = PersistentEntity::default();
 		let attacker = PersistentEntity::default();
-		app.world_mut().spawn((
-			attacker,
-			Attack(target),
-			_Enemy::new().with_mock(|mock| {
-				mock.expect_insert_attack()
-					.times(1)
-					.returning(|entity, attacker, target| {
-						entity.insert(_FakeAttack { attacker, target });
-					});
-				mock.expect_cool_down().return_const(Duration::ZERO);
-			}),
-		));
+		let entity = app
+			.world_mut()
+			.spawn((
+				attacker,
+				Attack(target),
+				_Enemy::new().with_mock(|mock| {
+					mock.expect_insert_attack()
+						.times(1)
+						.returning(|entity, attacker, target| {
+							entity.insert(_FakeAttack { attacker, target });
+						});
+					mock.expect_cool_down().return_const(Duration::ZERO);
+				}),
+			))
+			.id();
 
 		app.update();
 
+		let [child] = assert_count!(1, get_children!(app, entity));
 		assert_eq!(
-			Some(&_FakeAttack {
-				attacker: Attacker(attacker),
-				target: Target(target)
-			}),
-			app.world()
-				.iter_entities()
-				.find_map(|e| e.get::<_FakeAttack>())
+			(
+				Some(&_FakeAttack {
+					attacker: Attacker(attacker),
+					target: Target(target)
+				}),
+				Some(&Transform::default())
+			),
+			(child.get::<_FakeAttack>(), child.get::<Transform>(),)
+		);
+	}
+
+	#[test]
+	fn spawn_with_offset() {
+		let mut app = setup();
+		let target = PersistentEntity::default();
+		let attacker = PersistentEntity::default();
+		let entity = app
+			.world_mut()
+			.spawn((
+				attacker,
+				Attack(target),
+				GroundOffset(Vec3::new(1., 2., 3.)),
+				_Enemy::new().with_mock(|mock| {
+					mock.expect_insert_attack()
+						.times(1)
+						.returning(|entity, attacker, target| {
+							entity.insert(_FakeAttack { attacker, target });
+						});
+					mock.expect_cool_down().return_const(Duration::ZERO);
+				}),
+			))
+			.id();
+
+		app.update();
+
+		let [child] = assert_count!(1, get_children!(app, entity));
+		assert_eq!(
+			(
+				Some(&_FakeAttack {
+					attacker: Attacker(attacker),
+					target: Target(target)
+				}),
+				Some(&Transform::from_xyz(1., 2., 3.))
+			),
+			(child.get::<_FakeAttack>(), child.get::<Transform>(),)
 		);
 	}
 
 	#[test]
 	fn insert_cool_down() {
 		let mut app = setup();
-		let attacker = app
+		let entity = app
 			.world_mut()
 			.spawn((
 				PersistentEntity::default(),
@@ -151,14 +204,14 @@ mod tests {
 
 		assert_eq!(
 			Some(&OnCoolDown(Duration::from_secs(42))),
-			app.world().entity(attacker).get::<OnCoolDown>()
+			app.world().entity(entity).get::<OnCoolDown>()
 		);
 	}
 
 	#[test]
 	fn do_nothing_when_cool_down_present() {
 		let mut app = setup();
-		let attacker = app
+		let entity = app
 			.world_mut()
 			.spawn((
 				PersistentEntity::default(),
@@ -182,7 +235,7 @@ mod tests {
 				app.world()
 					.iter_entities()
 					.find_map(|e| e.get::<_FakeAttackEmpty>()),
-				app.world().entity(attacker).get::<OnCoolDown>()
+				app.world().entity(entity).get::<OnCoolDown>()
 			)
 		);
 	}
@@ -190,7 +243,7 @@ mod tests {
 	#[test]
 	fn despawn_attack_entity_when_attack_removed() {
 		let mut app = setup();
-		let attacker = app
+		let entity = app
 			.world_mut()
 			.spawn((
 				PersistentEntity::default(),
@@ -205,21 +258,16 @@ mod tests {
 			.id();
 
 		app.update();
-		app.world_mut().entity_mut(attacker).remove::<Attack>();
+		app.world_mut().entity_mut(entity).remove::<Attack>();
 		app.update();
 
-		assert_eq!(
-			None,
-			app.world()
-				.iter_entities()
-				.find_map(|e| e.get::<_FakeAttackEmpty>())
-		);
+		assert_count!(0, get_children!(app, entity));
 	}
 
 	#[test]
 	fn remove_ongoing_component_when_attack_removed() {
 		let mut app = setup();
-		let attacker = app
+		let entity = app
 			.world_mut()
 			.spawn((
 				PersistentEntity::default(),
@@ -232,12 +280,9 @@ mod tests {
 			.id();
 
 		app.update();
-		app.world_mut().entity_mut(attacker).remove::<Attack>();
+		app.world_mut().entity_mut(entity).remove::<Attack>();
 		app.update();
 
-		assert_eq!(
-			None,
-			app.world().iter_entities().find_map(|e| e.get::<Ongoing>())
-		);
+		assert_count!(0, get_children!(app, entity));
 	}
 }
