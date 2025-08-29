2b35fad5ae0650e6a65a50e27c20af3b125ec528
Codaishin <codaishin@gmail.com>
2025-08-29 17:03:07 +0200

    feat(core): add `Logger`

---
+++ src/zyheeda_core/src/logger.rs
@@ -0,0 +1,29 @@
+use tracing::{error, warn};
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Default)]
+pub struct Logger;
+
+impl Log for Logger {
+	fn log_waring<TError>(&self, value: TError)
+	where
+		TError: Into<String>,
+	{
+		warn!("{}", value.into())
+	}
+
+	fn log_error<TError>(&self, value: TError)
+	where
+		TError: Into<String>,
+	{
+		error!("{}", value.into());
+	}
+}
+
+pub trait Log {
+	fn log_waring<TError>(&self, value: TError)
+	where
+		TError: Into<String> + 'static;
+	fn log_error<TError>(&self, value: TError)
+	where
+		TError: Into<String> + 'static;
+}
