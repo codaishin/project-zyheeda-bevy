187c0010feda33cfe941d749245f807d5de22ff9
Codaishin <codaishin@gmail.com>
2025-09-04 11:15:32 +0200

    feat(testing): clear opposite `just_` states in `set_input`

--- src/testing/src/lib.rs
+++ src/testing/src/lib.rs
@@ -413,22 +413,26 @@ pub trait Mock {
 
 #[macro_export]
 macro_rules! set_input {
-	($app:expr, just_pressed($btn:expr)) => {
+	($app:expr, just_pressed($btn:expr)) => {{
 		let mut input = $app.world_mut().resource_mut::<ButtonInput<UserInput>>();
 		input.press($btn);
-	};
+		input.clear_just_released($btn);
+	}};
 	($app:expr, pressed($btn:expr)) => {{
 		let mut input = $app.world_mut().resource_mut::<ButtonInput<UserInput>>();
 		input.press($btn);
 		input.clear_just_pressed($btn);
+		input.clear_just_released($btn);
 	}};
 	($app:expr, just_released($btn:expr)) => {{
 		let mut input = $app.world_mut().resource_mut::<ButtonInput<UserInput>>();
 		input.release($btn);
+		input.clear_just_pressed($btn);
 	}};
 	($app:expr, released($btn:expr)) => {{
 		let mut input = $app.world_mut().resource_mut::<ButtonInput<UserInput>>();
 		input.release($btn);
+		input.clear_just_pressed($btn);
 		input.clear_just_released($btn);
 	}};
 	($app:expr, reset_all) => {{
