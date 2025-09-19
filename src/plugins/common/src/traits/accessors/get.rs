72f80c11e2315869dcd8786ea093de65e8c4bf88
Codaishin <codaishin@gmail.com>
2025-09-19 11:29:29 +0200

    feat(common): Add `GetProperty` and related traits

--- src/plugins/common/src/traits/accessors/get.rs
+++ src/plugins/common/src/traits/accessors/get.rs
@@ -134,6 +134,133 @@ pub trait TryApplyOn<'a, TKey>: GetMut<TKey> + 'a {
 
 impl<'a, T, TEntity> TryApplyOn<'a, TEntity> for T where T: GetMut<TEntity> + 'a {}
 
+/// A type-level descriptor for use with [`GetProperty`].
+///
+/// Implementors of this trait define how a value should be accessed through [`GetProperty`].
+/// The associated type [`TValue<'a>`](Property::TValue) determines whether the value is retrieved
+/// by reference (useful for computation-heavy or non-`Clone` types) or by value.
+///
+/// This can also be used for declarative wrapper types to expose their inner value in a
+/// specific way.
+/// # Example
+/// ```
+/// use common::traits::accessors::get::{Property};
+///
+/// // Small types can be returned by value.
+/// #[derive(Clone, Copy)]
+/// enum TinyEnum {
+///   A,
+///   B,
+/// }
+///
+/// impl Property for TinyEnum {
+///   type TValue<'a> = Self;
+/// }
+///
+/// // Large or expensive-to-clone types are better returned by reference.
+/// struct GiantArray([String; 100_000]);
+///
+/// impl Property for GiantArray {
+///   type TValue<'a> = &'a Self;
+/// }
+///
+/// // Wrapper types can choose to expose their inner value directly,
+/// // independent of how the value itself is stored.
+/// struct NumberOfLimbs(u8);
+///
+/// impl Property for NumberOfLimbs {
+///   type TValue<'a> = u8;
+/// }
+/// ```
+pub trait Property {
+	type TValue<'a>;
+}
+
+/// A generic getter trait over a [`Property`].
+///
+/// The property specifies how the value is exposed (by reference, by value, or via a transformed
+/// inner type). This allows consumers to request a value in a uniform way, while letting the
+/// property control the retrieval strategy.
+///
+/// # Example
+/// ```
+/// use common::traits::accessors::get::{Property, GetProperty};
+///
+/// #[derive(Debug, PartialEq)]
+/// struct MyProperty;
+///
+/// impl Property for MyProperty {
+///   type TValue<'a> = Self;
+/// }
+///
+/// struct Obj;
+///
+/// impl GetProperty<MyProperty> for Obj {
+///   fn get_property(&self) -> MyProperty {
+///     MyProperty
+///   }
+/// }
+///
+/// let obj = Obj;
+///
+/// assert_eq!(MyProperty, obj.get_property());
+/// ```
+pub trait GetProperty<TProperty>
+where
+	TProperty: Property,
+{
+	fn get_property(&self) -> TProperty::TValue<'_>;
+}
+
+/// A convenience trait to access any [`Property`] dynamically.
+///
+/// Useful when you want to name the retrieved property directly on the call.
+///
+/// ```
+/// use common::traits::accessors::get::{Property, GetProperty, DynProperty};
+///
+/// #[derive(Debug, PartialEq)]
+/// struct MyProperty;
+///
+/// impl Property for MyProperty {
+///   type TValue<'a> = Self;
+/// }
+///
+/// struct Obj;
+///
+/// impl GetProperty<MyProperty> for Obj {
+///   fn get_property(&self) -> MyProperty {
+///     MyProperty
+///   }
+/// }
+///
+/// let obj = Obj;
+///
+/// // when rust cannot determine which `GetProperty` impl of many to use:
+/// let a: MyProperty = obj.get_property();
+///
+/// // same issue as above, but using `DynProperty`:
+/// let b = obj.dyn_property::<MyProperty>();
+///
+/// assert_eq!(a, b);
+/// ```
+pub trait DynProperty {
+	fn dyn_property<TProperty>(&self) -> TProperty::TValue<'_>
+	where
+		TProperty: Property,
+		Self: GetProperty<TProperty>;
+}
+
+impl<T> DynProperty for T {
+	fn dyn_property<TProperty>(&self) -> TProperty::TValue<'_>
+	where
+		TProperty: Property,
+		Self: GetProperty<TProperty>,
+	{
+		GetProperty::<TProperty>::get_property(self)
+	}
+}
+
 /// A getter style conversion ("into" from a reference)
 ///
 /// A blanket implementation exists for types that implement `From<&SomeType>`, which - similar
