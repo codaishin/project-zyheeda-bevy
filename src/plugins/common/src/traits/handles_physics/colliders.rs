9679351a0ed76fee85bcade24f44b378267de55f
Codaishin <codaishin@gmail.com>
2025-12-10 16:01:57 +0100

    feat(common): add `Colliders` trait

---
+++ src/plugins/common/src/traits/handles_physics/colliders.rs
@@ -0,0 +1,61 @@
+use bevy::prelude::*;
+
+pub trait Colliders {
+	type TCollider: Component + From<Collider>;
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Collider {
+	pub center_offset: Vec3,
+	pub rotation: Quat,
+	pub shape: Shape,
+}
+
+impl Collider {
+	pub const fn from_shape(shape: Shape) -> Self {
+		Self {
+			shape,
+			center_offset: Vec3::ZERO,
+			rotation: Quat::IDENTITY,
+		}
+	}
+
+	pub const fn with_center_offset(mut self, center_offset: Vec3) -> Self {
+		self.center_offset = center_offset;
+		self
+	}
+
+	pub const fn with_rotation(mut self, rotation: Quat) -> Self {
+		self.rotation = rotation;
+		self
+	}
+}
+
+impl From<Shape> for Collider {
+	fn from(shape: Shape) -> Self {
+		Self::from_shape(shape)
+	}
+}
+
+/// Shape of a collider
+///
+/// Coordinates are in line with the bevy coordinate system.
+/// So without rotations they are:
+/// - `x`: width
+/// - `y`: height
+/// - `z`: depth
+#[derive(Debug, PartialEq)]
+pub enum Shape {
+	Sphere {
+		radius: f32,
+	},
+	Cuboid {
+		half_x: f32,
+		half_y: f32,
+		half_z: f32,
+	},
+	Capsule {
+		half_y: f32,
+		radius: f32,
+	},
+}
