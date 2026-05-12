cb44b09d79b807af5d09d5dce15ab17ad41a6c55
Codaishin <codaishin@gmail.com>
2026-05-12 14:35:40 +0200

    feat(light): override corridor light material

--- src/plugins/light/src/components/corridor_light.rs
+++ src/plugins/light/src/components/corridor_light.rs
@@ -1,10 +1,15 @@
 use crate::{components::light::Light, observers::get_insert_system::TargetMeshName};
 use bevy::{color::palettes::css::ANTIQUE_WHITE, prelude::*};
+use common::components::insert_asset::InsertAsset;
 use zyheeda_core::prelude::NormalizedNameLazy;
 
 #[derive(Component, Debug, PartialEq, Default)]
-#[require(Light, SpotLight = Self)]
+#[require(Light, SpotLight = Self, InsertAsset<StandardMaterial> = Self)]
 pub(crate) struct CorridorLight;
+
+impl CorridorLight {
+	const COLOR: Srgba = ANTIQUE_WHITE;
+}
 
 impl From<CorridorLight> for SpotLight {
 	fn from(_: CorridorLight) -> Self {
@@ -15,9 +20,19 @@ impl From<CorridorLight> for SpotLight {
 			inner_angle: f32::to_radians(0.),
 			outer_angle: f32::to_radians(89.),
 			shadows_enabled: false,
-			color: ANTIQUE_WHITE.into(),
+			color: CorridorLight::COLOR.into(),
 			..default()
 		}
+	}
+}
+
+impl From<CorridorLight> for InsertAsset<StandardMaterial> {
+	fn from(_: CorridorLight) -> Self {
+		InsertAsset::shared::<CorridorLight>(|| StandardMaterial {
+			base_color: CorridorLight::COLOR.into(),
+			emissive: (CorridorLight::COLOR * 5.).into(),
+			..default()
+		})
 	}
 }
 
