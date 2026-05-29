41dcd9c87175fce045708caf72e01b0e178f0fda
Codaishin <codaishin@gmail.com>
2026-05-29 12:13:32 +0200

    feat(interactive): impl `HandlesInteractive`

--- src/plugins/common/src/traits/handles_interactive.rs
+++ src/plugins/common/src/traits/handles_interactive.rs
@@ -1,17 +1,16 @@
 use crate::traits::{
 	accessors::get::{TryGetContext, View},
 	handles_map_generation::InteractiveType,
-	thread_safe::ThreadSafe,
 };
 use bevy::{ecs::system::SystemParam, prelude::*};
 use macros::EntityKey;
 
-pub trait HandlesInteractive: ThreadSafe {
+pub trait HandlesInteractive {
 	type TInteractive: SystemParam
 		+ for<'c> TryGetContext<Interactive, TContext<'c>: View<InteractiveType>>;
 }
 
 #[derive(EntityKey)]
 pub struct Interactive {
-	entity: Entity,
+	pub entity: Entity,
 }
---
+++ src/plugins/interactive/src/system_params/interactive_param/view_interactive_type.rs
@@ -0,0 +1,47 @@
+use crate::system_params::interactive_param::InteractiveContext;
+use common::traits::{accessors::get::View, handles_map_generation::InteractiveType};
+
+impl View<InteractiveType> for InteractiveContext<'_> {
+	fn view(&self) -> InteractiveType {
+		self.interactive.interactive_type
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{
+		components::interactive::Interactive,
+		system_params::interactive_param::InteractiveParam,
+	};
+	use bevy::{
+		ecs::system::{RunSystemError, RunSystemOnce},
+		prelude::*,
+	};
+	use common::traits::{
+		accessors::get::TryGetContext,
+		handles_interactive::Interactive as InteractiveKey,
+	};
+	use test_case::test_case;
+	use testing::SingleThreadedApp;
+
+	fn setup() -> App {
+		App::new().single_threaded(Update)
+	}
+
+	#[test_case(InteractiveType::Door;  "door")]
+	#[test_case(InteractiveType::Container;  "container")]
+	fn get_slide_door(interactive_type: InteractiveType) -> Result<(), RunSystemError> {
+		let mut app = setup();
+		let entity = app.world_mut().spawn(Interactive { interactive_type }).id();
+
+		let got = app
+			.world_mut()
+			.run_system_once(move |i: InteractiveParam| {
+				InteractiveParam::try_get_context(&i, InteractiveKey { entity }).map(|c| c.view())
+			})?;
+
+		assert_eq!(Some(interactive_type), got);
+		Ok(())
+	}
+}
