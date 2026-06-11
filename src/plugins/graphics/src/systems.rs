cecc900c68ac1e37162348fab16b2839ae9c27c4
Codaishin <codaishin@gmail.com>
2026-06-11 11:40:25 +0200

    fix(graphics): properly order render layer related systems
    fixes void spheres rendering when not on layer 0

--- src/plugins/graphics/src/systems.rs
+++ src/plugins/graphics/src/systems.rs
@@ -1,7 +1,6 @@
 pub(crate) mod init_post_process_pipeline;
+pub(crate) mod model_render_layers;
 pub(crate) mod modify_material;
 pub(crate) mod no_waiting_pipelines;
-pub(crate) mod populate_missing_model_render_layers;
 pub(crate) mod propagate_effect_material;
-pub(crate) mod propagate_model_render_layers;
 pub(crate) mod spawn_cameras;
---
+++ src/plugins/graphics/src/systems/model_render_layers.rs
@@ -0,0 +1,61 @@
+pub(crate) mod populate_missing;
+pub(crate) mod propagate;
+
+use crate::components::{
+	camera_labels::WorldPass,
+	child_meshes::ChildMeshOf,
+	model_render_layers::ModelRenderLayers,
+};
+use bevy::{ecs::system::ScheduleSystem, prelude::*};
+use common::systems::link::to_target::LinkToTarget;
+
+type UnlinkedMeshes = (Added<Mesh3d>, Without<ChildMeshOf>);
+
+impl ModelRenderLayers {
+	pub(crate) fn systems() -> impl IntoScheduleConfigs<ScheduleSystem, ()> {
+		(
+			ModelRenderLayers::populate_missing_with(WorldPass),
+			UnlinkedMeshes::link_to::<ModelRenderLayers, ChildMeshOf>,
+			ModelRenderLayers::propagate_layers,
+		)
+			.chain()
+	}
+}
+
+#[cfg(test)]
+mod regression_tests {
+	use super::*;
+	use bevy::camera::visibility::RenderLayers;
+	use testing::{SingleThreadedApp, new_handle};
+
+	fn setup() -> App {
+		let mut app = App::new().single_threaded(Update);
+
+		app.add_systems(Update, ModelRenderLayers::systems());
+
+		app
+	}
+
+	#[test]
+	fn propagate_manually_created_model() {
+		let mut app = setup();
+		let entity = app.world_mut().spawn(Visibility::Inherited).id();
+		let model = app
+			.world_mut()
+			.spawn((ChildOf(entity), Mesh3d(new_handle())))
+			.id();
+
+		app.update();
+
+		assert_eq!(
+			(
+				Some(&ModelRenderLayers::from(WorldPass)),
+				Some(&RenderLayers::from(WorldPass))
+			),
+			(
+				app.world().entity(entity).get::<ModelRenderLayers>(),
+				app.world().entity(model).get::<RenderLayers>()
+			)
+		);
+	}
+}
