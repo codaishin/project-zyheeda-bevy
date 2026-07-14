0dbcf4459e9d719be60386aad79f86c49c89757b
Codaishin <codaishin@gmail.com>
2026-07-14 16:14:18 +0200

    feat(movement): restart directional movement only when direction differs by at least one degree

--- src/plugins/movement/src/system_param/movement_param/start_movement.rs
+++ src/plugins/movement/src/system_param/movement_param/start_movement.rs
@@ -2,6 +2,8 @@ use crate::{components::movement::Movement, system_param::movement_param::Moveme
 use bevy::ecs::component::Component;
 use common::traits::handles_movement::{MovementTarget, StartMovement};
 
+const ALLOWED_ANGLE: f32 = 1_f32.to_radians();
+
 impl<TMotion> StartMovement for MovementContextMut<'_, TMotion>
 where
 	TMotion: Component,
@@ -10,8 +12,22 @@ where
 	where
 		T: Into<MovementTarget>,
 	{
-		self.entity.try_insert(Movement::from(target));
+		let movement = Movement::from(target);
+
+		if directions_within_tolerance(self.movement, &movement) {
+			return;
+		}
+
+		self.entity.try_insert(movement);
 	}
+}
+
+fn directions_within_tolerance(l: Option<&Movement>, r: &Movement) -> bool {
+	let (Some(Movement::Direction(l)), Movement::Direction(r)) = (l, r) else {
+		return false;
+	};
+
+	l.angle_between(**r) <= ALLOWED_ANGLE
 }
 
 #[cfg(test)]
@@ -30,18 +46,22 @@ mod tests {
 		thread_safe::ThreadSafe,
 	};
 	use test_case::test_case;
-	use testing::SingleThreadedApp;
+	use testing::{IsChanged, SingleThreadedApp};
 
 	#[derive(Component)]
 	struct _Motion;
 
 	fn setup() -> App {
-		App::new().single_threaded(Update)
+		let mut app = App::new().single_threaded(Update);
+
+		app.add_systems(Update, IsChanged::<Movement>::detect);
+
+		app
 	}
 
-	#[test_case(Vec3::new(1.,2.,3.); "to point")]
+	#[test_case(Vec3::new(1., 2., 3.); "to point")]
 	#[test_case(Dir3::NEG_X; "towards direction")]
-	fn insert_path(
+	fn insert_movement(
 		target: impl Into<MovementTarget> + Copy + ThreadSafe,
 	) -> Result<(), RunSystemError> {
 		let mut app = setup();
@@ -57,6 +77,87 @@ mod tests {
 
 		assert_eq!(
 			Some(&Movement::from(target)),
+			app.world().entity(entity).get::<Movement>(),
+		);
+		Ok(())
+	}
+
+	#[test]
+	fn do_not_insert_movement_direction_if_already_present() -> Result<(), RunSystemError> {
+		let mut app = setup();
+		let entity = app
+			.world_mut()
+			.spawn((Config::default(), Movement::from(Dir3::NEG_X)))
+			.id();
+
+		app.update();
+		app.world_mut()
+			.run_system_once(move |mut p: MovementParamMut<_Motion>| {
+				let mut ctx =
+					MovementParamMut::try_get_context_mut(&mut p, ConfiguredMovement { entity })
+						.unwrap();
+				ctx.start(Dir3::NEG_X);
+			})?;
+		app.update();
+
+		assert_eq!(
+			Some(&IsChanged::FALSE),
+			app.world().entity(entity).get::<IsChanged<Movement>>(),
+		);
+		Ok(())
+	}
+
+	#[test]
+	fn do_not_insert_movement_direction_if_already_present_within_tolerance()
+	-> Result<(), RunSystemError> {
+		let mut app = setup();
+		let dir = Vec3::NEG_X.rotate_axis(Vec3::Y, ALLOWED_ANGLE);
+		let dir = Dir3::try_from(dir).unwrap();
+		let entity = app
+			.world_mut()
+			.spawn((Config::default(), Movement::from(dir)))
+			.id();
+
+		app.update();
+		app.world_mut()
+			.run_system_once(move |mut p: MovementParamMut<_Motion>| {
+				let mut ctx =
+					MovementParamMut::try_get_context_mut(&mut p, ConfiguredMovement { entity })
+						.unwrap();
+				ctx.start(Dir3::NEG_X);
+			})?;
+		app.update();
+
+		assert_eq!(
+			Some(&IsChanged::FALSE),
+			app.world().entity(entity).get::<IsChanged<Movement>>(),
+		);
+		Ok(())
+	}
+
+	#[test]
+	fn re_insert_movement_direction_if_already_present_but_outside_tolerance()
+	-> Result<(), RunSystemError> {
+		let mut app = setup();
+		let dir = Vec3::NEG_X.rotate_axis(Vec3::Y, ALLOWED_ANGLE * 1.1);
+		let dir = Dir3::try_from(dir).unwrap();
+		let entity = app
+			.world_mut()
+			.spawn((Config::default(), Movement::from(dir)))
+			.id();
+
+		app.update();
+		app.world_mut()
+			.run_system_once(move |mut p: MovementParamMut<_Motion>| {
+				let mut ctx =
+					MovementParamMut::try_get_context_mut(&mut p, ConfiguredMovement { entity })
+						.unwrap();
+				ctx.start(Dir3::NEG_X);
+			})?;
+		app.update();
+
+		assert_eq!(
+			Some(&Movement::from(Dir3::NEG_X)),
 			app.world().entity(entity).get::<Movement>(),
 		);
 		Ok(())
