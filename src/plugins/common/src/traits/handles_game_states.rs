6560c57c28683289226eb8067d8293ea2e16746e
Codaishin <codaishin@gmail.com>
2026-08-06 18:02:21 +0200

    feat(common): add `HandlesGameStates`

---
+++ src/plugins/common/src/traits/handles_game_states.rs
@@ -0,0 +1,64 @@
+use bevy::{
+	ecs::system::{ScheduleSystem, SystemParam},
+	prelude::*,
+};
+use std::collections::HashSet;
+
+pub trait HandlesGameStates:
+	HandlesGameState<ActivityState> + HandlesGameState<SaveState> + HandlesGameState<MenuState>
+{
+}
+
+impl<T> HandlesGameStates for T where
+	T: HandlesGameState<ActivityState> + HandlesGameState<SaveState> + HandlesGameState<MenuState>
+{
+}
+
+pub trait HandlesGameState<T> {
+	type TGameStates: SystemParam + GameStates<T>;
+	type TGameStatesMut: SystemParam + GameStatesMut<T>;
+
+	fn add_systems<M>(
+		app: &mut App,
+		on_state: OnState<T>,
+		systems: impl IntoScheduleConfigs<ScheduleSystem, M>,
+	);
+}
+
+pub trait GameStates<T> {
+	fn game_states(&self) -> &HashSet<T>;
+}
+
+pub trait GameStatesMut<T>: GameStates<T> {
+	fn game_states_mut(&mut self) -> &mut HashSet<T>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum OnState<T> {
+	Enter(T),
+	Exit(T),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ActivityState {
+	LoadingEssentialAssets,
+	LoadDependencies,
+	NewGame,
+	Play,
+	Paused,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SaveState {
+	Save,
+	LoadAttempt,
+	Load,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MenuState {
+	StartMenu,
+	Inventory,
+	ComboOverview,
+	Settings,
+}
