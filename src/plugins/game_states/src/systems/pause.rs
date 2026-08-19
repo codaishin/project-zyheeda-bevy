252a42551b7027d683557e2b54eafb3f069c1218
Codaishin <codaishin@gmail.com>
2026-08-19 14:28:12 +0200

    refactor(game_states): reuse game state paused condition for pause control

--- src/plugins/game_states/src/systems/pause.rs
+++ src/plugins/game_states/src/systems/pause.rs
@@ -1,17 +1,9 @@
-use crate::{
-	resources::game_state_roles::GameStateRoles,
-	system_params::game_states_read::GameStatesRead,
-};
+use crate::resources::game_state_roles::GameStateRoles;
 use bevy::prelude::*;
-use common::prelude::*;
 
 impl GameStateRoles {
-	pub(crate) fn pause(
-		mut time: ResMut<Time<Virtual>>,
-		current: GameStatesRead,
-		roles: Res<GameStateRoles>,
-	) {
-		if current.iter().any(|state| roles.is_pause_state(state)) {
+	pub(crate) fn pause(In(paused): In<bool>, mut time: ResMut<Time<Virtual>>) {
+		if paused {
 			time.pause();
 		} else {
 			time.unpause();
@@ -22,34 +14,22 @@ impl GameStateRoles {
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::{
-		GameStatesPlugin,
-		resources::game_state_context::GameStateContext,
-		states::activity::ActivityState,
-		system_params::ui_states::UIStates,
-	};
-	use bevy::{state::app::StatesPlugin, time::TimePlugin};
+	use bevy::time::TimePlugin;
 	use testing::SingleThreadedApp;
 
-	fn setup() -> App {
+	fn setup(paused: bool) -> App {
 		let mut app = App::new().single_threaded(Update);
 
-		app.add_plugins(StatesPlugin);
 		app.add_plugins(TimePlugin);
-		UIStates::init(&mut app);
-		app.init_state::<ActivityState>();
-		app.init_resource::<GameStateContext>();
-		app.init_resource::<GameStateRoles>();
-		app.add_systems(Update, GameStateRoles::pause);
+		app.add_systems(Update, (move || paused).pipe(GameStateRoles::pause));
 
 		app
 	}
 
 	#[test]
 	fn pause() {
-		let mut app = setup();
-		app.world_mut().resource_mut::<GameStateContext>().activity =
-			Activity::Settable(SettableActivity::Paused);
+		let mut app = setup(true);
+		app.world_mut().resource_mut::<Time<Virtual>>().unpause();
 
 		app.update();
 
@@ -57,75 +37,9 @@ mod tests {
 	}
 
 	#[test]
-	fn un_pause_if_state_excluded_from_pause() {
-		let mut app = setup();
+	fn un_pause() {
+		let mut app = setup(false);
 		app.world_mut().resource_mut::<Time<Virtual>>().pause();
-		app.world_mut()
-			.resource_mut::<GameStateRoles>()
-			.non_pause_states
-			.insert(GameState::Activity(Activity::Settable(
-				SettableActivity::Paused,
-			)));
-		app.world_mut().resource_mut::<GameStateContext>().activity =
-			Activity::Settable(SettableActivity::Paused);
-
-		app.update();
-
-		assert!(!app.world().resource::<Time<Virtual>>().is_paused());
-	}
-
-	#[test]
-	fn default_non_pause_states_do_not_pause() {
-		let mut app = setup();
-
-		for state in GameStatesPlugin::DEFAULT {
-			app.world_mut().resource_mut::<Time<Virtual>>().pause();
-			app.world_mut().resource_mut::<GameStateContext>().activity = *state;
-
-			app.update();
-
-			assert!(!app.world().resource::<Time<Virtual>>().is_paused());
-		}
-	}
-
-	#[test]
-	fn pause_on_ui_state() {
-		let mut app = setup();
-		app.world_mut()
-			.resource_mut::<GameStateRoles>()
-			.non_pause_states
-			.insert(GameState::Activity(Activity::Settable(
-				SettableActivity::Paused,
-			)));
-		app.world_mut().resource_mut::<GameStateContext>().activity =
-			Activity::Settable(SettableActivity::Paused);
-		app.world_mut()
-			.resource_mut::<GameStateContext>()
-			.ui
-			.insert(IngameUI::Hud);
-
-		app.update();
-
-		assert!(app.world().resource::<Time<Virtual>>().is_paused());
-	}
-
-	#[test]
-	fn un_pause_if_ui_state_excluded_from_pause() {
-		let mut app = setup();
-		app.world_mut().resource_mut::<Time<Virtual>>().pause();
-		app.world_mut()
-			.resource_mut::<GameStateRoles>()
-			.non_pause_states
-			.extend([
-				GameState::Activity(Activity::Settable(SettableActivity::Paused)),
-				GameState::IngameUI(IngameUI::Hud),
-			]);
-		app.world_mut().resource_mut::<GameStateContext>().activity =
-			Activity::Settable(SettableActivity::Paused);
-		app.world_mut()
-			.resource_mut::<GameStateContext>()
-			.ui
-			.insert(IngameUI::Hud);
 
 		app.update();
 
