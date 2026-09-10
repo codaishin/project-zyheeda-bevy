465a06fb2cad08313baf796cef0222232a2e5360
Codaishin <codaishin@gmail.com>
2026-09-10 19:06:56 +0200

    feat(physics): decay impact

--- src/plugins/physics/src/components/impacted.rs
+++ src/plugins/physics/src/components/impacted.rs
@@ -3,7 +3,34 @@ use common::tools::vec_not_nan::VecNotNan;
 use std::collections::HashMap;
 use zyheeda_core::prelude::*;
 
+pub(crate) type ImpactStrength = F32FiniteStrictlyPositive;
+
 #[derive(Component, Debug, PartialEq, Default)]
 pub(crate) struct Impacted {
-	pub(crate) impact_points: HashMap<VecNotNan<3>, F32FinitePositive>,
+	pub(crate) impact_points: HashMap<VecNotNan<3>, ImpactStrength>,
+}
+
+#[cfg(test)]
+impl testing::ApproxEqual<f32> for Impacted {
+	fn approx_equal(&self, other: &Self, tolerance: &f32) -> bool {
+		if other
+			.impact_points
+			.keys()
+			.any(|k| !self.impact_points.contains_key(k))
+		{
+			return false;
+		}
+
+		for (point, s_strength) in &self.impact_points {
+			let Some(o_strength) = other.impact_points.get(point) else {
+				return false;
+			};
+
+			if !s_strength.approx_equal(o_strength, tolerance) {
+				return false;
+			}
+		}
+
+		true
+	}
 }
--- src/plugins/physics/src/systems/cast_rays/apply_beam_impacts.rs
+++ src/plugins/physics/src/systems/cast_rays/apply_beam_impacts.rs
@@ -1,19 +1,25 @@
-use std::collections::HashMap;
-
 use crate::components::{
 	cast_rays::{CastRayFor, CastRays, RayCastResult},
+	collider::ColliderOf,
 	impacted::Impacted,
 };
 use bevy::prelude::*;
 use common::prelude::*;
+use std::collections::HashMap;
 use zyheeda_core::prelude::*;
 
 impl CastRays {
-	pub(crate) fn apply_beam_impacts(cast_rays: Query<&Self>, mut impacted: Query<&mut Impacted>) {
+	pub(crate) fn apply_beam_impacts(
+		cast_rays: Query<&Self>,
+		mut impacted: Query<&mut Impacted>,
+		child_colliders: Query<&ColliderOf>,
+	) {
 		for CastRays { results } in cast_rays {
 			let Some((hit, point)) = Self::get_beam_impact(results) else {
 				continue;
 			};
+
+			let hit = Self::get_root(hit, child_colliders);
 
 			let Ok(mut impacted) = impacted.get_mut(hit) else {
 				continue;
@@ -21,7 +27,7 @@ impl CastRays {
 
 			impacted
 				.impact_points
-				.insert(point, new_f32!(F32FinitePositive(1.0)));
+				.insert(point, new_f32!(F32FiniteStrictlyPositive(1.0)));
 		}
 	}
 
@@ -35,6 +41,13 @@ impl CastRays {
 			VecNotNan::try_from(result.args.origin + result.args.direction * *result.toi).ok()?,
 		))
 	}
+
+	fn get_root(entity: Entity, child_colliders: Query<&ColliderOf>) -> Entity {
+		match child_colliders.get(entity) {
+			Ok(ColliderOf(root)) => *root,
+			Err(_) => entity,
+		}
+	}
 }
 
 #[cfg(test)]
@@ -42,6 +55,7 @@ mod tests {
 	use super::*;
 	use crate::components::{
 		cast_rays::{CastRayFor, RayCastResult, RayCasterArgs},
+		collider::ColliderOf,
 		impacted::Impacted,
 	};
 	use std::collections::HashMap;
@@ -80,10 +94,43 @@ mod tests {
 			Some(&Impacted {
 				impact_points: HashMap::from([(
 					vec_not_nan!(1., 13., 3.),
-					new_f32!(F32FinitePositive(1.))
+					new_f32!(F32FiniteStrictlyPositive(1.))
 				)])
 			}),
 			app.world().entity(hit).get::<Impacted>(),
+		);
+	}
+
+	#[test]
+	fn insert_impact_on_root() {
+		let mut app = setup();
+		let root = app.world_mut().spawn(Impacted::default()).id();
+		let hit = app.world_mut().spawn(ColliderOf(root)).id();
+		app.world_mut().spawn(CastRays {
+			results: HashMap::from([(
+				CastRayFor::Beam,
+				RayCastResult {
+					hit: Some(hit),
+					toi: toi!(11.),
+					args: RayCasterArgs {
+						origin: Vec3::new(1., 2., 3.),
+						direction: Dir3::Y,
+						..default()
+					},
+				},
+			)]),
+		});
+
+		app.update();
+
+		assert_eq!(
+			Some(&Impacted {
+				impact_points: HashMap::from([(
+					vec_not_nan!(1., 13., 3.),
+					new_f32!(F32FiniteStrictlyPositive(1.))
+				)])
+			}),
+			app.world().entity(root).get::<Impacted>(),
 		);
 	}
 }
